d4c7d6dbcd885f34996b6a36c8c2a8c9ebc5af2a
Brian Sumner <brian.sumner@amd.com>
2017-02-16 08:12:13 -0800

    Use correct format for unsigned long

--- test_conformance/half/cl_utils.c
+++ test_conformance/half/cl_utils.c
@@ -455,7 +455,7 @@ size_t getBufferSize(cl_device_id device_id)
         }
         if (result > BUFFER_SIZE)
             result = BUFFER_SIZE;
-        log_info("Using const buffer size 0x%lx (%ld)\n", (unsigned long)result, (unsigned long)result);
+        log_info("Using const buffer size 0x%lx (%lu)\n", (unsigned long)result, (unsigned long)result);
         err = clGetDeviceInfo (device_id,
                                CL_DEVICE_GLOBAL_MEM_SIZE,
                                sizeof(maxGlobalSize), (void *)&maxGlobalSize,
