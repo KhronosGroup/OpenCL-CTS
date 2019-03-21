d7409e1075f42ccb64b1c36fc1afbd549ee98b58
Grzegorz Wawiorko <grzegorz.wawiorko@intel.com>
2019-03-21 16:19:01 +0100

    Improved CRC calculation

---
+++ test_common/harness/crc32.h
@@ -0,0 +1,34 @@
+/******************************************************************
+Copyright (c) 2016 The Khronos Group Inc.
+All Rights Reserved.  This code is protected by copyright laws and
+contains material proprietary to the Khronos Group, Inc.
+This is UNPUBLISHED PROPRIETARY SOURCE CODE that may not be disclosed
+in whole or in part to third parties, and may not be reproduced, republished,
+distributed, transmitted, displayed, broadcast or otherwise exploited in any
+manner without the express prior written permission of Khronos Group.
+
+The receipt or possession of this code does not convey any rights to
+reproduce, disclose, or distribute its contents, or to
+manufacture, use, or sell anything that it may describe, in whole
+or in part other than under the terms of the Khronos Adopters
+Agreement or Khronos Conformance Test Source License Agreement as
+executed between Khronos and the recipient.
+******************************************************************/
+
+#ifndef _CRC32_H_
+#define _CRC32_H_
+
+#include <stdint.h> 
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint32_t crc32(uint32_t crc, const void *buf, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
