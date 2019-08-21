13a156d1d6f0815d361049b06a5e6589aa2dc458
Grzegorz Wawiorko <grzegorz.wawiorko@intel.com>
2019-08-21 14:58:07 +0200

    Fix verification if atomics 64bit extensions supported by device

--- test_conformance/SVM/common.h
+++ test_conformance/SVM/common.h
@@ -21,6 +21,7 @@
 #include "harness/errorHelpers.h"
 #include "harness/kernelHelpers.h"
 #include "harness/typeWrappers.h"
+#include <vector>
 
 #if (defined(_WIN32) || defined(_WIN64)) && defined(_MSC_VER)
     #include <windows.h>
@@ -94,7 +95,7 @@ extern int    test_svm_shared_sub_buffers(cl_device_id deviceID, cl_context cont
 extern int    test_svm_enqueue_api(cl_device_id deviceID, cl_context context, cl_command_queue queue, int num_elements);
 extern int    test_svm_migrate(cl_device_id deviceID, cl_context context, cl_command_queue queue, int num_elements);
 
-extern cl_int create_cl_objects(cl_device_id device_from_harness, const char** ppCodeString, cl_context* context, cl_program *program, cl_command_queue *queues, cl_uint *num_devices, cl_device_svm_capabilities required_svm_caps);
+extern cl_int create_cl_objects(cl_device_id device_from_harness, const char** ppCodeString, cl_context* context, cl_program *program, cl_command_queue *queues, cl_uint *num_devices, cl_device_svm_capabilities required_svm_caps, std::vector<std::string> extensions_list = {});
 
 extern const char *linked_list_create_and_verify_kernels[];
 
