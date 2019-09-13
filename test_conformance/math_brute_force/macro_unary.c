c5cf03d9ee24678ba1a62e2a03adb6b7a2c2a725
Jim Lewis <j.lewis1@samsung.com>
2019-09-13 14:20:23 -0500

    Fix build: move declaration out of exit block

--- test_conformance/math_brute_force/macro_unary.c
+++ test_conformance/math_brute_force/macro_unary.c
@@ -384,6 +384,7 @@ static cl_int TestFloat( cl_uint job_id, cl_uint thread_id, void *data )
     int     ftz = job->ftz;
     cl_uint j, k;
     cl_int error = CL_SUCCESS;
+    cl_int ret = CL_SUCCESS;
     const char *name = job->f->name;
 
     int signbit_test = 0;
@@ -552,7 +553,7 @@ static cl_int TestFloat( cl_uint job_id, cl_uint thread_id, void *data )
     }
 
 exit:
-    cl_int ret = error;
+    ret = error;
     for( j = gMinVectorSizeIndex; j < gMaxVectorSizeIndex; j++ )
     {
         if( (error = clEnqueueUnmapMemObject( tinfo->tQueue, tinfo->outBuf[j], out[j], 0, NULL, NULL)) )
