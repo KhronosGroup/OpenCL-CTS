57f00faa2031c26a9a948166467c42fd466cd8e3
Boaz Ouriel <boaz.ouriel@gmail.com>
2020-02-07 18:45:46 +0200

    conditional execution of the pipe test suite

--- test_conformance/pipes/main.c
+++ test_conformance/pipes/main.c
@@ -1,6 +1,6 @@
 //
 // Copyright (c) 2017 The Khronos Group Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -15,72 +15,89 @@
 //
 #include "harness/compat.h"
 
+#include "harness/testHarness.h"
+#include "procs.h"
 #include <stdio.h>
 #include <string.h>
-#include "procs.h"
-#include "harness/testHarness.h"
+
+test_status InitCL(cl_device_id device) {
+  int error;
+  cl_uint max_packet_size;
+  error = clGetDeviceInfo(device, CL_DEVICE_PIPE_MAX_PACKET_SIZE,
+                          sizeof(max_packet_size), &max_packet_size, NULL);
+  if (error != CL_SUCCESS) {
+    print_error(error, "Unable to get pipe max packet size");
+    return TEST_FAIL;
+  }
+
+  if (max_packet_size == 0) {
+    return TEST_SKIP;
+  }
+
+  return TEST_PASS;
+}
 
 test_definition test_list[] = {
-    ADD_TEST_VERSION( pipe_readwrite_int, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_readwrite_uint, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_readwrite_long, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_readwrite_ulong, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_readwrite_short, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_readwrite_ushort, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_readwrite_float, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_readwrite_half, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_readwrite_char, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_readwrite_uchar, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_readwrite_double, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_readwrite_struct, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_workgroup_readwrite_int, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_workgroup_readwrite_uint, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_workgroup_readwrite_long, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_workgroup_readwrite_ulong, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_workgroup_readwrite_short, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_workgroup_readwrite_ushort, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_workgroup_readwrite_float, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_workgroup_readwrite_half, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_workgroup_readwrite_char, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_workgroup_readwrite_uchar, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_workgroup_readwrite_double, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_workgroup_readwrite_struct, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_subgroup_readwrite_int, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_subgroup_readwrite_uint, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_subgroup_readwrite_long, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_subgroup_readwrite_ulong, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_subgroup_readwrite_short, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_subgroup_readwrite_ushort, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_subgroup_readwrite_float, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_subgroup_readwrite_half, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_subgroup_readwrite_char, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_subgroup_readwrite_uchar, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_subgroup_readwrite_double, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_subgroup_readwrite_struct, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_convenience_readwrite_int, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_convenience_readwrite_uint, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_convenience_readwrite_long, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_convenience_readwrite_ulong, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_convenience_readwrite_short, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_convenience_readwrite_ushort, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_convenience_readwrite_float, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_convenience_readwrite_half, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_convenience_readwrite_char, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_convenience_readwrite_uchar, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_convenience_readwrite_double, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_convenience_readwrite_struct, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_info, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_max_args, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_max_packet_size, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_max_active_reservations, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_query_functions, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_readwrite_errors, Version(2, 0) ),
-    ADD_TEST_VERSION( pipe_subgroups_divergence, Version(2, 0) ),
+    ADD_TEST_VERSION(pipe_readwrite_int, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_readwrite_uint, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_readwrite_long, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_readwrite_ulong, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_readwrite_short, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_readwrite_ushort, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_readwrite_float, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_readwrite_half, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_readwrite_char, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_readwrite_uchar, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_readwrite_double, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_readwrite_struct, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_workgroup_readwrite_int, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_workgroup_readwrite_uint, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_workgroup_readwrite_long, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_workgroup_readwrite_ulong, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_workgroup_readwrite_short, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_workgroup_readwrite_ushort, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_workgroup_readwrite_float, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_workgroup_readwrite_half, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_workgroup_readwrite_char, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_workgroup_readwrite_uchar, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_workgroup_readwrite_double, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_workgroup_readwrite_struct, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_subgroup_readwrite_int, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_subgroup_readwrite_uint, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_subgroup_readwrite_long, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_subgroup_readwrite_ulong, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_subgroup_readwrite_short, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_subgroup_readwrite_ushort, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_subgroup_readwrite_float, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_subgroup_readwrite_half, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_subgroup_readwrite_char, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_subgroup_readwrite_uchar, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_subgroup_readwrite_double, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_subgroup_readwrite_struct, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_convenience_readwrite_int, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_convenience_readwrite_uint, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_convenience_readwrite_long, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_convenience_readwrite_ulong, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_convenience_readwrite_short, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_convenience_readwrite_ushort, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_convenience_readwrite_float, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_convenience_readwrite_half, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_convenience_readwrite_char, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_convenience_readwrite_uchar, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_convenience_readwrite_double, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_convenience_readwrite_struct, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_info, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_max_args, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_max_packet_size, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_max_active_reservations, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_query_functions, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_readwrite_errors, Version(2, 0)),
+    ADD_TEST_VERSION(pipe_subgroups_divergence, Version(2, 0)),
 };
 
-const int test_num = ARRAY_SIZE( test_list );
+const int test_num = ARRAY_SIZE(test_list);
 
-int main( int argc, const char *argv[] )
-{
-    return runTestHarness( argc, argv, test_num, test_list, false, false, 0 );
+int main(int argc, const char *argv[]) {
+  return runTestHarnessWithCheck(argc, argv, test_num, test_list, false, false,
+                                 0, InitCL);
 }
