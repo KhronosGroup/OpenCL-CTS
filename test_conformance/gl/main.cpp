5286901e89a12fc984723caf2045238c4749b8e6
Jimbo <jiabaxie@amd.com>
2020-02-19 15:59:49 -0500

    Cleaned up some redundant code

--- test_conformance/gl/main.cpp
+++ test_conformance/gl/main.cpp
@@ -153,8 +153,6 @@ test_definition test_list32[] = {
 const int test_num = ARRAY_SIZE( test_list );
 const int test_num32 = ARRAY_SIZE( test_list32 );
 
-cl_device_type gDeviceType = CL_DEVICE_TYPE_DEFAULT;
-
 int main(int argc, const char *argv[])
 {
   gTestRounding = true;
@@ -170,7 +168,6 @@ int main(int argc, const char *argv[])
   }	
 
   cl_device_type requestedDeviceType = CL_DEVICE_TYPE_DEFAULT;
-  checkDeviceTypeOverride(&requestedDeviceType);
 
   /* Do we have a CPU/GPU specification? */
   if( argc > 1 )
