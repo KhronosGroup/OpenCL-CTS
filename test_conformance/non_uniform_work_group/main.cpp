cc0774f376bab5e2e1cfa86946cae5b6915e81be
Grzegorz Wawiorko <grzegorz.wawiorko@intel.com>
2020-03-18 12:38:07 +0100

    Fix - add TEST_PASS test status in group of tests

--- test_conformance/non_uniform_work_group/main.cpp
+++ test_conformance/non_uniform_work_group/main.cpp
@@ -46,6 +46,7 @@ test_status InitCL(cl_device_id device) {
         version_expected_info("Test", expected_min_version.to_string().c_str(), version.to_string().c_str());
         return TEST_SKIP;
     }
+    return TEST_PASS;
 }
 
 int main(int argc, const char *argv[])
