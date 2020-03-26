d128fea964b66c9e553f28183558968a12108d7b
John Kesapides <46718829+JohnKesapidesARM@users.noreply.github.com>
2020-03-26 22:14:46 +0000

    Global variable gDeviceType correctly set (#611)
    Add gDeviceType set on kernel_read_write tests(image_streams)
    as the device type affects the way validation is performed in the test.
    
    Signed-off-by: John Kesapides <john.kesapides@arm.com>

--- test_common/harness/testHarness.cpp
+++ test_common/harness/testHarness.cpp
@@ -253,7 +253,7 @@ int runTestHarnessWithCheck( int argc, const char *argv[], int testNum, test_def
         }
     }
 
- 	gDeviceType = device_type;
+
 
 	switch (device_type)
 	{
@@ -344,6 +344,13 @@ int runTestHarnessWithCheck( int argc, const char *argv[], int testNum, test_def
 
     device = devices[choosen_device_index];
 
+    err = clGetDeviceInfo( device, CL_DEVICE_TYPE, sizeof(gDeviceType), &gDeviceType, NULL );
+    if( err )
+    {
+        print_error( err, "Unable to get device type" );
+        return TEST_FAIL;
+    }
+    
     if( printDeviceHeader( device ) != CL_SUCCESS )
     {
         return EXIT_FAILURE;
