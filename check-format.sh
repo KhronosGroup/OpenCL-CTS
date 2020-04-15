9090e9bea18d0ef358d3d4ba78681bd628b8e22f
James Price <jrprice@google.com>
2020-04-15 12:16:49 -0400

    Use clang-format-9 for Travis CI formatting checks

--- check-format.sh
+++ check-format.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # Run git-clang-format to check for violations
-CLANG_FORMAT_OUTPUT=$(git-clang-format --diff origin/master --extensions c,cpp,h,hpp)
+if [ "$TRAVIS" == "true" ]; then
+    EXTRA_OPTS="--binary `which clang-format-9`"
+fi
+CLANG_FORMAT_OUTPUT=$(git-clang-format --diff origin/master --extensions c,cpp,h,hpp $EXTRA_OPTS)
 
 # Check for no-ops
 grep '^no modified files to format$' <<<"$CLANG_FORMAT_OUTPUT" && exit 0
