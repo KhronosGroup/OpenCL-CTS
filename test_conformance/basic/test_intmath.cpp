5a58c2a0884774cbbd6a64dedda410facee0f474
James Price <jrprice@google.com>
2020-05-01 12:45:16 -0400

    Refactor to remove duplicate verification code
    Remove need for hardcoded number of test operations.

--- test_conformance/basic/test_intmath.cpp
+++ test_conformance/basic/test_intmath.cpp
@@ -14,42 +14,76 @@
 // limitations under the License.
 //
 
+#include <functional>
 #include <string>
 #include <vector>
 
 #include "procs.h"
 
-const char *add_kernel_code = R"(
+template <typename T> struct TestDef
+{
+    const char *name;
+    const char *kernel_code;
+    std::function<T(T, T, T)> ref;
+};
+
+template <typename T, unsigned N>
+int test_intmath(cl_device_id device, cl_context context,
+                 cl_command_queue queue, int num_elements, std::string typestr)
+{
+    TestDef<T> tests[] = {
+        // Test addition
+        {
+            "test_add",
+            R"(
   __kernel void test_add(__global TYPE *srcA,
                          __global TYPE *srcB,
+                         __global TYPE *srcC,
                          __global TYPE *dst)
   {
       int  tid = get_global_id(0);
       dst[tid] = srcA[tid] + srcB[tid];
   };
-)";
+)",
+            [](T a, T b, T c) { return a + b; },
+        },
 
-const char *sub_kernel_code = R"(
+        // Test subtraction
+        {
+            "test_sub",
+            R"(
   __kernel void test_sub(__global TYPE *srcA,
                          __global TYPE *srcB,
+                         __global TYPE *srcC,
                          __global TYPE *dst)
   {
       int  tid = get_global_id(0);
       dst[tid] = srcA[tid] - srcB[tid];
   };
-)";
+)",
+            [](T a, T b, T c) { return a - b; },
+        },
 
-const char *mul_kernel_code = R"(
+        // Test multiplication
+        {
+            "test_mul",
+            R"(
   __kernel void test_mul(__global TYPE *srcA,
                          __global TYPE *srcB,
+                         __global TYPE *srcC,
                          __global TYPE *dst)
   {
       int  tid = get_global_id(0);
       dst[tid] = srcA[tid] * srcB[tid];
   };
-)";
+)",
+            [](T a, T b, T c) { return a * b; },
+        },
 
-const char *mad_kernel_code = R"(
+        // Test multiply-accumulate
+        {
+            "test_mad",
+            R"(
   __kernel void test_mad(__global TYPE *srcA,
                          __global TYPE *srcB,
                          __global TYPE *srcC,
@@ -58,87 +92,12 @@ const char *mad_kernel_code = R"(
       int  tid = get_global_id(0);
       dst[tid] = srcA[tid] * srcB[tid] + srcC[tid];
   };
-)";
+)",
+            [](T a, T b, T c) { return a * b + c; },
+        },
+    };
 
-template <typename T>
-int verify_add(const std::vector<T> &inputA, const std::vector<T> &inputB,
-               const std::vector<T> &output)
-{
-    for (int i = 0; i < output.size(); i++)
-    {
-        T r = inputA[i] + inputB[i];
-        if (r != output[i])
-        {
-            log_error("ADD test failed\n");
-            return -1;
-        }
-    }
-
-    log_info("ADD test passed\n");
-    return 0;
-}
-
-template <typename T>
-int verify_sub(const std::vector<T> &inputA, const std::vector<T> &inputB,
-               const std::vector<T> &output)
-{
-    for (int i = 0; i < output.size(); i++)
-    {
-        T r = inputA[i] - inputB[i];
-        if (r != output[i])
-        {
-            log_error("SUB test failed\n");
-            return -1;
-        }
-    }
-
-    log_info("SUB test passed\n");
-    return 0;
-}
-
-template <typename T>
-int verify_mul(const std::vector<T> &inputA, const std::vector<T> &inputB,
-               const std::vector<T> &output)
-{
-    for (int i = 0; i < output.size(); i++)
-    {
-        T r = inputA[i] * inputB[i];
-        if (r != output[i])
-        {
-            log_error("MUL test failed\n");
-            return -1;
-        }
-    }
-
-    log_info("MUL test passed\n");
-    return 0;
-}
-
-template <typename T>
-int verify_mad(const std::vector<T> &inputA, const std::vector<T> &inputB,
-               const std::vector<T> &inputC, const std::vector<T> &output)
-{
-    for (T i = 0; i < output.size(); i++)
-    {
-        T r = inputA[i] * inputB[i] + inputC[i];
-        if (r != output[i])
-        {
-            log_error("MAD test failed\n");
-            return -1;
-        }
-    }
-
-    log_info("MAD test passed\n");
-    return 0;
-}
-
-template <typename T, unsigned N>
-int test_intmath(cl_device_id device, cl_context context,
-                 cl_command_queue queue, int num_elements, std::string typestr)
-{
     clMemWrapper streams[4];
-    clProgramWrapper program[4];
-    clKernelWrapper kernel[4];
     cl_int err;
 
     if (std::is_same<T, cl_ulong>::value && !gHasLong)
@@ -182,65 +141,50 @@ int test_intmath(cl_device_id device, cl_context context,
                                inputC.data(), 0, NULL, NULL);
     test_error(err, "clEnqueueWriteBuffer failed\n");
 
-    // Build kernels.
     std::string build_options = "-DTYPE=";
     build_options += typestr;
-    err = create_single_kernel_helper(context, &program[0], &kernel[0], 1,
-                                      &add_kernel_code, "test_add",
-                                      build_options.c_str());
-    test_error(err, "create_single_kernel_helper failed\n");
-    err = create_single_kernel_helper(context, &program[1], &kernel[1], 1,
-                                      &sub_kernel_code, "test_sub",
-                                      build_options.c_str());
-    test_error(err, "create_single_kernel_helper failed\n");
-    err = create_single_kernel_helper(context, &program[2], &kernel[2], 1,
-                                      &mul_kernel_code, "test_mul",
-                                      build_options.c_str());
-    test_error(err, "create_single_kernel_helper failed\n");
-    err = create_single_kernel_helper(context, &program[3], &kernel[3], 1,
-                                      &mad_kernel_code, "test_mad",
-                                      build_options.c_str());
-    test_error(err, "create_single_kernel_helper failed\n");
 
-    err = clSetKernelArg(kernel[0], 0, sizeof(cl_mem), &streams[0]);
-    err |= clSetKernelArg(kernel[0], 1, sizeof(cl_mem), &streams[1]);
-    err |= clSetKernelArg(kernel[0], 2, sizeof(cl_mem), &streams[3]);
-    test_error(err, "clSetKernelArgs failed\n");
-
-    err = clSetKernelArg(kernel[1], 0, sizeof(cl_mem), &streams[0]);
-    err |= clSetKernelArg(kernel[1], 1, sizeof(cl_mem), &streams[1]);
-    err |= clSetKernelArg(kernel[1], 2, sizeof(cl_mem), &streams[3]);
-    test_error(err, "clSetKernelArgs failed\n");
-
-    err = clSetKernelArg(kernel[2], 0, sizeof(cl_mem), &streams[0]);
-    err |= clSetKernelArg(kernel[2], 1, sizeof(cl_mem), &streams[1]);
-    err |= clSetKernelArg(kernel[2], 2, sizeof(cl_mem), &streams[3]);
-    test_error(err, "clSetKernelArgs failed\n");
-
-    err = clSetKernelArg(kernel[3], 0, sizeof(cl_mem), &streams[0]);
-    err |= clSetKernelArg(kernel[3], 1, sizeof(cl_mem), &streams[1]);
-    err |= clSetKernelArg(kernel[3], 2, sizeof(cl_mem), &streams[2]);
-    err |= clSetKernelArg(kernel[3], 3, sizeof(cl_mem), &streams[3]);
-    test_error(err, "clSetKernelArgs failed\n");
-
-    size_t threads[1] = { static_cast<size_t>(num_elements) };
-    for (int i = 0; i < 4; i++)
+    // Run test for each operation
+    for (auto test : tests)
     {
-        err = clEnqueueNDRangeKernel(queue, kernel[i], 1, NULL, threads, NULL,
-                                     0, NULL, NULL);
+        log_info("%s... ", test.name);
+
+        // Create kernel and set args
+        clProgramWrapper program;
+        clKernelWrapper kernel;
+        err = create_single_kernel_helper(context, &program, &kernel, 1,
+                                          &test.kernel_code, test.name,
+                                          build_options.c_str());
+        test_error(err, "create_single_kernel_helper failed\n");
+
+        err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &streams[0]);
+        err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &streams[1]);
+        err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &streams[2]);
+        err |= clSetKernelArg(kernel, 3, sizeof(cl_mem), &streams[3]);
+        test_error(err, "clSetKernelArgs failed\n");
+
+        // Run kernel
+        size_t threads[1] = { static_cast<size_t>(num_elements) };
+        err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, threads, NULL, 0,
+                                     NULL, NULL);
         test_error(err, "clEnqueueNDRangeKernel failed\n");
 
+        // Read results
         err = clEnqueueReadBuffer(queue, streams[3], CL_TRUE, 0, datasize,
                                   output.data(), 0, NULL, NULL);
         test_error(err, "clEnqueueReadBuffer failed\n");
 
-        switch (i)
+        // Verify results
+        for (int i = 0; i < num_elements * N; i++)
         {
-            case 0: err = verify_add(inputA, inputB, output); break;
-            case 1: err = verify_sub(inputA, inputB, output); break;
-            case 2: err = verify_mul(inputA, inputB, output); break;
-            case 3: err = verify_mad(inputA, inputB, inputC, output); break;
+            T r = test.ref(inputA[i], inputB[i], inputC[i]);
+            if (r != output[i])
+            {
+                log_error("failed\n");
+                return -1;
+            }
         }
+        log_info("passed\n");
     }
 
     return err;
