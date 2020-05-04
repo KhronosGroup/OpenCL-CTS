6b4d66a03c564cba685735ad8080fc17fe21e936
Ellen Norris-Thompson <ellen.norris-thompson@arm.com>
2020-05-04 16:42:48 +0100

    Add argument to specify branch for clang-format check
    If a change is not added to an origin/master branch, an argument
    can be used to specify the relevant branch name for the clang-format
    comparison.
    
    Change-Id: I8ce712d077bf4d048a04a29c12bc2cb45e4abb93
    Signed-off-by: Ellen Norris-Thompson <ellen.norris-thompson@arm.com>

--- check-format.sh
+++ check-format.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+# Arg used to specify non-'origin/master' comparison branch
+ORIGIN_BRANCH=${1:-"origin/master"}
+
 # Run git-clang-format to check for violations
 if [ "$TRAVIS" == "true" ]; then
     EXTRA_OPTS="--binary `which clang-format-9`"
 fi
-CLANG_FORMAT_OUTPUT=$(git-clang-format --diff origin/master --extensions c,cpp,h,hpp $EXTRA_OPTS)
+CLANG_FORMAT_OUTPUT=$(git-clang-format --diff $ORIGIN_BRANCH --extensions c,cpp,h,hpp $EXTRA_OPTS)
 
 # Check for no-ops
 grep '^no modified files to format$' <<<"$CLANG_FORMAT_OUTPUT" && exit 0
