4948d942bd119968b7bc866885585ae9b244adca
Guang <891528583@qq.com>
2020-09-09 16:19:08 +0800

    fix gles build error

--- test_common/gles/gl_headers.h
+++ test_common/gles/gl_headers.h
@@ -27,7 +27,6 @@
 #endif
 
 #include <GLES2/gl2ext.h>
-#include <GLES2/gl2extQCOM.h>
 
 // Some macros to minimize the changes in the tests from GL to GLES2
 #define glGenRenderbuffersEXT        glGenRenderbuffers
@@ -40,21 +39,33 @@
 #define glDeleteFramebuffersEXT      glDeleteFramebuffers
 #define glBindFramebufferEXT         glBindFramebuffer
 #define glFramebufferRenderbufferEXT glFramebufferRenderbuffer
+
+#ifndef GL_ES_VERSION_3_0
 #define glTexImage3D                 glTexImage3DOES
+#define GL_RGBA32F                   GL_RGBA32F_EXT
+//#define GL_TEXTURE_3D                GL_TEXTURE_3D_OES
+#define GL_READ_ONLY                 GL_BUFFER_ACCESS_OES
+#define GL_HALF_FLOAT_ARB            GL_HALF_FLOAT_OES
+#define GL_BGRA                      GL_BGRA_EXT
+#define glMapBufferRange             glMapBufferRangeEXT
+#define glUnmapBuffer                glUnmapBufferOES
+#else
+//#define GL_HALF_FLOAT_OES            GL_HALF_FLOAT
+//#define GL_RGBA8_OES                 GL_RGBA8
+#define GL_HALF_FLOAT_ARB            GL_HALF_FLOAT
+#endif
+
 #define glutGetProcAddress           eglGetProcAddress
 
 #define GL_FRAMEBUFFER_EXT           GL_FRAMEBUFFER
 #define GL_FRAMEBUFFER_COMPLETE_EXT  GL_FRAMEBUFFER_COMPLETE
 #define GL_RENDERBUFFER_INTERNAL_FORMAT_EXT GL_RENDERBUFFER_INTERNAL_FORMAT
 #define GL_RENDERBUFFER_EXT          GL_RENDERBUFFER
-#define GL_COLOR_ATTACHMENT0_EXT     GL_COLOR_ATTACHMENT0
+//#define GL_COLOR_ATTACHMENT0_EXT     GL_COLOR_ATTACHMENT0
 #define GL_DEPTH_ATTACHMENT_EXT      GL_DEPTH_ATTACHMENT
-#define GL_TEXTURE_3D                GL_TEXTURE_3D_OES
-#define GL_READ_ONLY                 GL_BUFFER_ACCESS_OES
 
-#define GL_HALF_FLOAT_ARB            GL_HALF_FLOAT_OES
-#define GL_BGRA                      GL_BGRA_EXT
 #define GL_RGBA32F_ARB               GL_RGBA
+#define GL_BGRA                      GL_BGRA_EXT
 
 typedef unsigned short GLhalf;
 
