d9c8b3cd8028526082f5ae02aba641b5e1a6bba6
Ben Ashbaugh <ben.ashbaugh@intel.com>
2020-09-24 08:52:18 -0700

    temporarily disable the unload_invalid test case
    Different implementations of the OpenCL ICD loader treat a null
    platform differently, so this case does not reliably return
    CL_INVALID_PLATFORM.

--- test_conformance/compiler/main.cpp
+++ test_conformance/compiler/main.cpp
@@ -90,7 +90,7 @@ test_definition test_list[] = {
     ADD_TEST_VERSION(pragma_unroll, Version(2, 0)),
 
     ADD_TEST(unload_valid),
-    ADD_TEST(unload_invalid),
+    // ADD_TEST(unload_invalid), // disabling temporarily, see GitHub #977
     ADD_TEST(unload_repeated),
     ADD_TEST(unload_compile_unload_link),
     ADD_TEST(unload_build_unload_create_kernel),
