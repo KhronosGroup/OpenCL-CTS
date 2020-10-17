4a3cb4e068f2672a2db04c34c6483a4f9ceacca4
Guang <891528583@qq.com>
2020-10-17 09:18:25 +0800

    fix Werror and link error

--- test_common/gles/gl_headers.h
+++ test_common/gles/gl_headers.h
@@ -24,6 +24,9 @@
 #include <GLES3/gl3.h>
 #else
 #include <GLES2/gl2.h>
+#define glTexImage3DOES glTexImage3D
+#define glUnmapBufferOES glUnmapBuffer
+#define glMapBufferRangeEXT glMapBufferRange
 #endif
 
 #include <GLES2/gl2ext.h>
@@ -41,17 +44,11 @@
 #define glFramebufferRenderbufferEXT glFramebufferRenderbuffer
 
 #ifndef GL_ES_VERSION_3_0
-#define glTexImage3D                 glTexImage3DOES
 #define GL_RGBA32F GL_RGBA32F_EXT
-//#define GL_TEXTURE_3D                GL_TEXTURE_3D_OES
 #define GL_READ_ONLY GL_BUFFER_ACCESS_OES
 #define GL_HALF_FLOAT_ARB GL_HALF_FLOAT_OES
 #define GL_BGRA GL_BGRA_EXT
-#define glMapBufferRange glMapBufferRangeEXT
-#define glUnmapBuffer glUnmapBufferOES
 #else
-//#define GL_HALF_FLOAT_OES            GL_HALF_FLOAT
-//#define GL_RGBA8_OES                 GL_RGBA8
 #define GL_HALF_FLOAT_ARB GL_HALF_FLOAT
 #endif
 
@@ -61,7 +58,6 @@
 #define GL_FRAMEBUFFER_COMPLETE_EXT  GL_FRAMEBUFFER_COMPLETE
 #define GL_RENDERBUFFER_INTERNAL_FORMAT_EXT GL_RENDERBUFFER_INTERNAL_FORMAT
 #define GL_RENDERBUFFER_EXT          GL_RENDERBUFFER
-//#define GL_COLOR_ATTACHMENT0_EXT     GL_COLOR_ATTACHMENT0
 #define GL_DEPTH_ATTACHMENT_EXT      GL_DEPTH_ATTACHMENT
 
 #define GL_RGBA32F_ARB               GL_RGBA
