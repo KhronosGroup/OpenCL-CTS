eb3da7a5b46bb4386ac8cf3646923eca7f314fbc
Radek Szymanski <radek.szymanski@arm.com>
2020-11-04 09:17:54 +0000

    Remove unused code in clGetInfo
    These declarations either aren't used or aren't needed, as testBase.h
    already declares them.
    
    Signed-off-by: Radek Szymanski <radek.szymanski@arm.com>

--- test_conformance/images/clGetInfo/test_1D.cpp
+++ test_conformance/images/clGetInfo/test_1D.cpp
@@ -15,11 +15,6 @@
 //
 #include "../testBase.h"
 
-#define MAX_ERR 0.005f
-#define MAX_HALF_LINEAR_ERR 0.3f
-
-extern bool            gDebugTrace, gTestSmallImages, gTestMaxImages;
-
 extern int test_get_image_info_single( cl_context context, image_descriptor *imageInfo, MTdata d, cl_mem_flags flags, size_t row_pitch, size_t slice_pitch );
 
 
