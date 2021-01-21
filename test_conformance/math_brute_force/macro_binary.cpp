5424fa527e8840c5ab75e4935db0cb1519bdc88b
Marco Antognini <marco.antognini@arm.com>
2021-01-21 11:36:01 +0000

    Make functions local to translation unit
    Remove unnecessary declarations.
    
    Signed-off-by: Marco Antognini <marco.antognini@arm.com>

--- test_conformance/math_brute_force/macro_binary.cpp
+++ test_conformance/math_brute_force/macro_binary.cpp
@@ -24,11 +24,6 @@ int TestMacro_Int_Double_Double(const Func *f, MTdata, bool relaxedMode);
 extern const vtbl _macro_binary = { "macro_binary", TestMacro_Int_Float_Float,
                                     TestMacro_Int_Double_Double };
 
-static int BuildKernel(const char *name, int vectorSize, cl_uint kernel_count,
-                       cl_kernel *k, cl_program *p);
-static int BuildKernelDouble(const char *name, int vectorSize,
-                             cl_uint kernel_count, cl_kernel *k, cl_program *p,
-                             bool relaxedMode);
 
 static int BuildKernel(const char *name, int vectorSize, cl_uint kernel_count,
                        cl_kernel *k, cl_program *p, bool relaxedMode)
@@ -215,8 +210,6 @@ typedef struct BuildKernelInfo
 } BuildKernelInfo;
 
 static cl_int BuildKernel_FloatFn(cl_uint job_id, cl_uint thread_id UNUSED,
-                                  void *p);
-static cl_int BuildKernel_FloatFn(cl_uint job_id, cl_uint thread_id UNUSED,
                                   void *p)
 {
     BuildKernelInfo *info = (BuildKernelInfo *)p;
@@ -225,8 +218,6 @@ static cl_int BuildKernel_FloatFn(cl_uint job_id, cl_uint thread_id UNUSED,
                        info->kernels[i], info->programs + i, info->relaxedMode);
 }
 
-static cl_int BuildKernel_DoubleFn(cl_uint job_id, cl_uint thread_id UNUSED,
-                                   void *p);
 static cl_int BuildKernel_DoubleFn(cl_uint job_id, cl_uint thread_id UNUSED,
                                    void *p)
 {
--- test_conformance/math_brute_force/ternary.cpp
+++ test_conformance/math_brute_force/ternary.cpp
@@ -29,10 +29,6 @@ extern const vtbl _ternary = { "ternary", TestFunc_Float_Float_Float_Float,
                                TestFunc_Double_Double_Double_Double };
 
 static int BuildKernel(const char *name, int vectorSize, cl_kernel *k,
-                       cl_program *p, bool relaxedMode);
-static int BuildKernelDouble(const char *name, int vectorSize, cl_kernel *k,
-                             cl_program *p, bool relaxedMode);
-static int BuildKernel(const char *name, int vectorSize, cl_kernel *k,
                        cl_program *p, bool relaxedMode)
 {
     const char *c[] = { "__kernel void math_kernel",
@@ -222,8 +218,6 @@ typedef struct BuildKernelInfo
 } BuildKernelInfo;
 
 static cl_int BuildKernel_FloatFn(cl_uint job_id, cl_uint thread_id UNUSED,
-                                  void *p);
-static cl_int BuildKernel_FloatFn(cl_uint job_id, cl_uint thread_id UNUSED,
                                   void *p)
 {
     BuildKernelInfo *info = (BuildKernelInfo *)p;
@@ -232,8 +226,6 @@ static cl_int BuildKernel_FloatFn(cl_uint job_id, cl_uint thread_id UNUSED,
                        info->programs + i, info->relaxedMode);
 }
 
-static cl_int BuildKernel_DoubleFn(cl_uint job_id, cl_uint thread_id UNUSED,
-                                   void *p);
 static cl_int BuildKernel_DoubleFn(cl_uint job_id, cl_uint thread_id UNUSED,
                                    void *p)
 {
