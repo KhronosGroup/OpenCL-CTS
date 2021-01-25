8c3c1ebdef82762e2f73557c686765b9f4883f7a
Ellen Norris-Thompson <ellen.norris-thompson@arm.com>
2021-01-25 17:26:00 +0000

    Removing unused code
    Tidying commented out or unnecessary code
    
    Contributes #616
    
    Signed-off-by: Ellen Norris-Thompson <ellen.norris-thompson@arm.com>

--- test_conformance/images/kernel_read_write/test_common.h
+++ test_conformance/images/kernel_read_write/test_common.h
@@ -143,82 +143,67 @@ int determine_validation_error_offset(
     }
     if (!clampingErr)
     {
-        /*        if( clamped && ( (int)x + (int)xOffsetValues[ j ] < 0 ||
-         (int)y + (int)yOffsetValues[ j ] < 0 ) )
-         {
-         log_error( "NEGATIVE COORDINATE ERROR\n" );
-         return -1;
-         }
-         */
-        if (true) // gExtraValidateInfo )
+        if (printAsFloat)
         {
-            if (printAsFloat)
-            {
-                log_error("Sample %ld: coord {%f(%a),%f(%a),%f(%a)} did not "
-                          "validate!\n\tExpected (%g,%g,%g,%g),\n\t     got "
-                          "(%g,%g,%g,%g), error of %g\n",
-                          j, x, x, y, y, z, z, (float)expected[0],
-                          (float)expected[1], (float)expected[2],
-                          (float)expected[3], (float)resultPtr[0],
-                          (float)resultPtr[1], (float)resultPtr[2],
-                          (float)resultPtr[3], error);
-            }
-            else
-            {
-                log_error("Sample %ld: coord {%f(%a),%f(%a),%f(%a)} did not "
-                          "validate!\n\tExpected (%x,%x,%x,%x),\n\t     got "
-                          "(%x,%x,%x,%x)\n",
-                          j, x, x, y, y, z, z, (int)expected[0],
-                          (int)expected[1], (int)expected[2], (int)expected[3],
-                          (int)resultPtr[0], (int)resultPtr[1],
-                          (int)resultPtr[2], (int)resultPtr[3]);
-            }
-            log_error(
-                "Integer coords resolve to %d,%d,%d   with img size %d,%d,%d\n",
-                clampedX, clampedY, clampedZ, (int)imageWidth, (int)imageHeight,
-                (int)imageDepth);
+            log_error("Sample %ld: coord {%f(%a),%f(%a),%f(%a)} did not "
+                      "validate!\n\tExpected (%g,%g,%g,%g),\n\t     got "
+                      "(%g,%g,%g,%g), error of %g\n",
+                      j, x, x, y, y, z, z, (float)expected[0],
+                      (float)expected[1], (float)expected[2],
+                      (float)expected[3], (float)resultPtr[0],
+                      (float)resultPtr[1], (float)resultPtr[2],
+                      (float)resultPtr[3], error);
+        }
+        else
+        {
+            log_error("Sample %ld: coord {%f(%a),%f(%a),%f(%a)} did not "
+                      "validate!\n\tExpected (%x,%x,%x,%x),\n\t     got "
+                      "(%x,%x,%x,%x)\n",
+                      j, x, x, y, y, z, z, (int)expected[0], (int)expected[1],
+                      (int)expected[2], (int)expected[3], (int)resultPtr[0],
+                      (int)resultPtr[1], (int)resultPtr[2], (int)resultPtr[3]);
+        }
+        log_error(
+            "Integer coords resolve to %d,%d,%d   with img size %d,%d,%d\n",
+            clampedX, clampedY, clampedZ, (int)imageWidth, (int)imageHeight,
+            (int)imageDepth);
 
-            if (printAsFloat && gExtraValidateInfo)
+        if (printAsFloat && gExtraValidateInfo)
+        {
+            log_error("\nNearby values:\n");
+            for (int zOff = -1; zOff <= 1; zOff++)
             {
-                log_error("\nNearby values:\n");
-                for (int zOff = -1; zOff <= 1; zOff++)
+                for (int yOff = -1; yOff <= 1; yOff++)
                 {
-                    for (int yOff = -1; yOff <= 1; yOff++)
-                    {
-                        float top[4], real[4], bot[4];
-                        read_image_pixel_float(imagePtr, imageInfo,
-                                               clampedX - 1, clampedY + yOff,
-                                               clampedZ + zOff, top);
-                        read_image_pixel_float(imagePtr, imageInfo, clampedX,
-                                               clampedY + yOff, clampedZ + zOff,
-                                               real);
-                        read_image_pixel_float(imagePtr, imageInfo,
-                                               clampedX + 1, clampedY + yOff,
-                                               clampedZ + zOff, bot);
-                        log_error("\t(%g,%g,%g,%g)", top[0], top[1], top[2],
-                                  top[3]);
-                        log_error(" (%g,%g,%g,%g)", real[0], real[1], real[2],
-                                  real[3]);
-                        log_error(" (%g,%g,%g,%g)\n", bot[0], bot[1], bot[2],
-                                  bot[3]);
-                    }
+                    float top[4], real[4], bot[4];
+                    read_image_pixel_float(imagePtr, imageInfo, clampedX - 1,
+                                           clampedY + yOff, clampedZ + zOff,
+                                           top);
+                    read_image_pixel_float(imagePtr, imageInfo, clampedX,
+                                           clampedY + yOff, clampedZ + zOff,
+                                           real);
+                    read_image_pixel_float(imagePtr, imageInfo, clampedX + 1,
+                                           clampedY + yOff, clampedZ + zOff,
+                                           bot);
+                    log_error("\t(%g,%g,%g,%g)", top[0], top[1], top[2],
+                              top[3]);
+                    log_error(" (%g,%g,%g,%g)", real[0], real[1], real[2],
+                              real[3]);
+                    log_error(" (%g,%g,%g,%g)\n", bot[0], bot[1], bot[2],
+                              bot[3]);
                 }
             }
-            //        }
-            //        else
-            //            log_error( "\n" );
-            if (imageSampler->filter_mode != CL_FILTER_LINEAR)
-            {
-                if (found)
-                    log_error(
-                        "\tValue really found in image at %d,%d,%d (%s)\n",
-                        actualX, actualY, actualZ,
-                        (found > 1) ? "NOT unique!!" : "unique");
-                else
-                    log_error("\tValue not actually found in image\n");
-            }
-            log_error("\n");
         }
+        if (imageSampler->filter_mode != CL_FILTER_LINEAR)
+        {
+            if (found)
+                log_error("\tValue really found in image at %d,%d,%d (%s)\n",
+                          actualX, actualY, actualZ,
+                          (found > 1) ? "NOT unique!!" : "unique");
+            else
+                log_error("\tValue not actually found in image\n");
+        }
+        log_error("\n");
 
         numClamped = -1; // We force the clamped counter to never work
         if ((--numTries) == 0) return -1;
