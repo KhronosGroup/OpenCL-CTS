6f0b3f4a6a99ed1b835072aef9bdc014810d28f0
Ben Ashbaugh <ben.ashbaugh@intel.com>
2021-03-01 17:29:40 -0800

    added test for bitfield insert

--- test_conformance/integer_ops/test_extended_bit_ops.cpp
+++ test_conformance/integer_ops/test_extended_bit_ops.cpp
@@ -71,42 +71,61 @@ static void generate_input(std::vector<T>& base)
 }
 
 template <typename T>
-static T cpu_bit_reverse(T base)
+static T cpu_bit_insert(T tbase, T tinsert, cl_uint offset, cl_uint count)
 {
-    T result = 0;
+    assert(offset <= sizeof(T) * 8);
+    assert(count <= sizeof(T) * 8);
+    assert(offset + count <= sizeof(T) * 8);
 
-    const size_t count = sizeof(T) * 8;
-    for (size_t i = 0; i < count; i++) {
-        if (base & ((T)1 << i)) {
-            result |= ((T)1 << (count - i - 1));
-        }
-    }
-    return result;
+    cl_ulong base = static_cast<cl_ulong>(tbase);
+    cl_ulong insert = static_cast<cl_ulong>(tinsert);
+
+    cl_ulong mask = (count < 64) ? ((1ULL << count) - 1) << offset : ~0ULL;
+    cl_ulong result = ((insert << offset) & mask) | (base & ~mask);
+
+    return static_cast<T>(result);
 }
 
-template <typename T>
-static void calculate_reference(std::vector<T>& ref, const std::vector<T>& base)
+template <typename T, size_t N>
+static void calculate_reference(std::vector<T>& ref, const std::vector<T>& base, const std::vector<T>& insert)
 {
     ref.resize(base.size());
     for (size_t i = 0; i < base.size(); i++) {
-        ref[i] = cpu_bit_reverse(base[i]);
+        cl_uint offset = (i / N) / (sizeof(T) * 8 + 1);
+        cl_uint count = (i / N) % (sizeof(T) * 8 + 1);
+        if (offset + count > sizeof(T) * 8)
+        {
+            count = (sizeof(T) * 8) - offset;
+        }
+        ref[i] = cpu_bit_insert(base[i], insert[i], offset, count);
     }
 }
 
 static constexpr const char* kernel_source = R"CLC(
-__kernel void test_bit_reverse(__global TYPE* dst, __global TYPE* base)
+__kernel void test_bitfield_insert(__global TYPE* dst, __global TYPE* base, __global TYPE* insert)
 {
     int index = get_global_id(0);
-    dst[index] = bit_reverse(base[index]);
+    uint offset = index / (sizeof(BASETYPE) * 8 + 1);
+    uint count = index % (sizeof(BASETYPE) * 8 + 1);
+    if (offset + count > sizeof(BASETYPE) * 8) {
+        count = (sizeof(BASETYPE) * 8) - offset;
+    }
+    dst[index] = bitfield_insert(base[index], insert[index], offset, count);
 }
 )CLC";
 
 static constexpr const char* kernel_source_vec3 = R"CLC(
-__kernel void test_bit_reverse(__global BASETYPE* dst, __global BASETYPE* base)
+__kernel void test_bitfield_insert(__global BASETYPE* dst, __global BASETYPE* base, __global BASETYPE* insert)
 {
     int index = get_global_id(0);
-    TYPE s = vload3(index, base);
-    TYPE d = bit_reverse(s);
+    uint offset = index / (sizeof(BASETYPE) * 8 + 1);
+    uint count = index % (sizeof(BASETYPE) * 8 + 1);
+    if (offset + count > sizeof(BASETYPE) * 8) {
+        count = (sizeof(BASETYPE) * 8) - offset;
+    }
+    TYPE b = vload3(index, base);
+    TYPE i = vload3(index, insert);
+    TYPE d = bitfield_insert(b, i, offset, count);
     vstore3(d, index, dst);
 }
 )CLC";
@@ -130,37 +149,50 @@ static int test_vectype(const char* type_name, cl_device_id device,
     buildOptions += " -DBASETYPE=";
     buildOptions += type_name;
     // TEMP: delete this when we've switched names!
-    buildOptions += " -Dcl_intel_bit_instructions -Dbit_reverse=intel_bfrev";
+    buildOptions += " -Dcl_intel_bit_instructions -Dbitfield_insert=intel_bfi";
 
-    const size_t ELEMENTS_TO_TEST = 65536;
+    const size_t ELEMENTS_TO_TEST = (sizeof(T) * 8 + 1) * (sizeof(T) * 8 + 1);
+
     std::vector<T> base(ELEMENTS_TO_TEST * N);
-    generate_input(base);
+    std::fill(base.begin(), base.end(), static_cast<T>(0xA5A5A5A5A5A5A5A5ULL));
+
+    std::vector<T> insert(ELEMENTS_TO_TEST * N);
+    generate_input(insert);
 
     std::vector<T> reference;
-    calculate_reference(reference, base);
+    calculate_reference<T, N>(reference, base, insert);
 
     const char* source = (N == 3) ? kernel_source_vec3 : kernel_source;
     error = create_single_kernel_helper(
-        context, &program, &kernel, 1, &source, "test_bit_reverse",
+        context, &program, &kernel, 1, &source, "test_bitfield_insert",
         buildOptions.c_str());
-    test_error(error, "Unable to create test_bit_reverse kernel");
+    test_error(error, "Unable to create test_bitfield_insert kernel");
 
-    clMemWrapper src;
     clMemWrapper dst;
+    clMemWrapper src_base;
+    clMemWrapper src_insert;
 
     dst =
         clCreateBuffer(context, 0, reference.size() * sizeof(T), NULL, &error);
     test_error(error, "Unable to create output buffer");
 
-    src = clCreateBuffer(context, CL_MEM_COPY_HOST_PTR,
-                         base.size() * sizeof(T), base.data(), &error);
+    src_base = clCreateBuffer(context, CL_MEM_COPY_HOST_PTR,
+                              base.size() * sizeof(T), base.data(), &error);
     test_error(error, "Unable to create base buffer");
+
+    src_insert =
+        clCreateBuffer(context, CL_MEM_COPY_HOST_PTR, insert.size() * sizeof(T),
+                       insert.data(), &error);
+    test_error(error, "Unable to create insert buffer");
 
     error = clSetKernelArg(kernel, 0, sizeof(dst), &dst);
     test_error(error, "Unable to set output buffer kernel arg");
 
-    error = clSetKernelArg(kernel, 1, sizeof(src), &src);
+    error = clSetKernelArg(kernel, 1, sizeof(src_base), &src_base);
     test_error(error, "Unable to set base buffer kernel arg");
+
+    error = clSetKernelArg(kernel, 2, sizeof(src_insert), &src_insert);
+    test_error(error, "Unable to set insert buffer kernel arg");
 
     size_t global_work_size[] = { reference.size() / N };
     error = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, global_work_size,
@@ -178,6 +210,19 @@ static int test_vectype(const char* type_name, cl_device_id device,
 
     if (results != reference)
     {
+#if 1
+        for (size_t i = 0; i < reference.size(); i++) {
+            if (results[i] != reference[i]) {
+                cl_uint offset = (i / N) / (sizeof(T) * 8 + 1);
+                cl_uint count = (i / N) % (sizeof(T) * 8 + 1);
+                if (offset + count > sizeof(T) * 8)
+                {
+                    count = (sizeof(T) * 8) - offset;
+                }
+                printf("At index %zu: wanted %llX, got %llX: base = %llX, insert = %llX, offset = %u, count = %u.\n", i, reference[i], results[i], base[i], insert[i], offset, count);
+            }
+        }
+#endif
         log_error("Result buffer did not match reference buffer!\n");
         return TEST_FAIL;
     }
@@ -202,23 +247,22 @@ static int test_type(const char* type_name, cl_device_id device,
 int test_extended_bit_ops_insert(cl_device_id device, cl_context context,
                                  cl_command_queue queue, int num_elements)
 {
-    if (is_extension_available(device, "cl_khr_extended_bit_ops"))
+    // TODO: add back this check!
+    if (true || is_extension_available(device, "cl_khr_extended_bit_ops"))
     {
         int result = TEST_PASS;
 
-#if 0
-        result |= test_type<cl_char>("char", device, context, queue);
+        //result |= test_type<cl_char>("char", device, context, queue);
         result |= test_type<cl_uchar>("uchar", device, context, queue);
-        result |= test_type<cl_short>("short", device, context, queue);
+        //result |= test_type<cl_short>("short", device, context, queue);
         result |= test_type<cl_ushort>("ushort", device, context, queue);
-        result |= test_type<cl_int>("int", device, context, queue);
+        //result |= test_type<cl_int>("int", device, context, queue);
         result |= test_type<cl_uint>("uint", device, context, queue);
         if (gHasLong)
         {
-            result |= test_type<cl_long>("long", device, context, queue);
+        //    result |= test_type<cl_long>("long", device, context, queue);
             result |= test_type<cl_ulong>("ulong", device, context, queue);
         }
-#endif
         return result;
     }
 
