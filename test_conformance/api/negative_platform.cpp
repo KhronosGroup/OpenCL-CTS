32ba98fefe53f67ddcf9a43318ba1be5623d5a27
Chetankumar Mistry <chetan.mistry@arm.com>
2021-03-12 09:00:06 +0000

    [SQUASH] Implement Object Validity Testing Argument
    Add functionality in the harness to allow negative
    tests to optionally test Object Validity by passing in
    a valid object which is not the correct object.
    This option can be selected by passing
    --invalid-object-scenarios as a command-line argument.
    
    Additionally this change updates the negative platform
    tests so that they use this feature
    
    Signed-off-by: Chetankumar Mistry <chetan.mistry@arm.com>

--- test_conformance/api/negative_platform.cpp
+++ test_conformance/api/negative_platform.cpp
@@ -42,15 +42,17 @@ int test_negative_get_platform_info(cl_device_id deviceID, cl_context context,
 {
     cl_platform_id platform = getPlatformFromDevice(deviceID);
 
-    cl_int err =
-        clGetPlatformInfo(reinterpret_cast<cl_platform_id>(deviceID),
-                          CL_PLATFORM_VERSION, sizeof(char*), nullptr, nullptr);
-    test_failure_error_ret(
-        err, CL_INVALID_PLATFORM,
-        "clGetPlatformInfo should return CL_INVALID_PLATFORM  when: \"platform "
-        "is not a valid platform\" using a valid object which is NOT a "
-        "platform",
-        TEST_FAIL);
+    cl_int err(CL_SUCCESS);
+    for (auto invalid_platform : get_invalid_objects<cl_platform_id>(deviceID))
+    {
+        err = clGetPlatformInfo(invalid_platform, CL_PLATFORM_VERSION,
+                                sizeof(char*), nullptr, nullptr);
+        test_failure_error_ret(err, CL_INVALID_PLATFORM,
+                               "clGetPlatformInfo should return "
+                               "CL_INVALID_PLATFORM  when: \"platform "
+                               "is not a valid platform\"",
+                               TEST_FAIL);
+    }
 
     constexpr cl_platform_info INVALID_PARAM_VALUE = 0;
     err = clGetPlatformInfo(platform, INVALID_PARAM_VALUE, 0, nullptr, nullptr);
