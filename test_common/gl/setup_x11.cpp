97d29217f869ed2b0e045c8b4928a04e4516a35e
Grzegorz Wawiorko <grzegorz.wawiorko@intel.com>
2021-10-04 15:36:13 +0200

    Fix build, glext should not be used with GLEW

--- test_common/gl/setup_x11.cpp
+++ test_common/gl/setup_x11.cpp
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-#define GL_GLEXT_PROTOTYPES
 
 #include "setup.h"
 #include "testBase.h"
@@ -21,7 +20,6 @@
 
 #include <GL/gl.h>
 #include <GL/glut.h>
-#include <GL/glext.h>
 #include <GL/freeglut.h>
 #include <GL/glx.h>
 #include <CL/cl_ext.h>
