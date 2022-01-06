91057505c90b8a01ec080f8a8af50c2e5975ce73
Sreelakshmi Haridas <sharidas@quicinc.com>
2022-01-05 19:23:40 -0700

    Formatting fixes

--- test_conformance/basic/test_vector_swizzle.cpp
+++ test_conformance/basic/test_vector_swizzle.cpp
@@ -631,8 +631,8 @@ static int test_vectype(const char* type_name, cl_device_id device,
 
         const char* xyzw_source = TestInfo<N>::kernel_source_xyzw;
         error = create_single_kernel_helper(
-            context, &program, &kernel, 1, &xyzw_source, "test_vector_swizzle_xyzw",
-            buildOptions.c_str());
+            context, &program, &kernel, 1, &xyzw_source,
+            "test_vector_swizzle_xyzw", buildOptions.c_str());
         test_error(error, "Unable to create xyzw test kernel");
 
         result |= test_vectype_case(value, reference, context, kernel, queue);
@@ -643,10 +643,10 @@ static int test_vectype(const char* type_name, cl_device_id device,
         clProgramWrapper program;
         clKernelWrapper kernel;
 
-        const char *sN_source = TestInfo<N>::kernel_source_sN;
-        error = create_single_kernel_helper(context, &program, &kernel, 1,
-                                            &sN_source, "test_vector_swizzle_sN",
-                                            buildOptions.c_str());
+        const char* sN_source = TestInfo<N>::kernel_source_sN;
+        error = create_single_kernel_helper(
+            context, &program, &kernel, 1, &sN_source, "test_vector_swizzle_sN",
+            buildOptions.c_str());
         test_error(error, "Unable to create sN test kernel");
 
         result |= test_vectype_case(value, reference, context, kernel, queue);
@@ -660,13 +660,14 @@ static int test_vectype(const char* type_name, cl_device_id device,
         const Version device_version = get_device_cl_version(device);
         if (device_version >= Version(3, 0))
         {
-            const char *rgba_source = TestInfo<N>::kernel_source_rgba;
+            const char* rgba_source = TestInfo<N>::kernel_source_rgba;
             error = create_single_kernel_helper(
                 context, &program, &kernel, 1, &rgba_source,
                 "test_vector_swizzle_rgba", buildOptions.c_str());
             test_error(error, "Unable to create rgba test kernel");
 
-            result |= test_vectype_case(value, reference, context, kernel, queue);
+            result |=
+                test_vectype_case(value, reference, context, kernel, queue);
         }
     }
 
