cc721778545d3d07c350684a7dae1f3ef55d02e9
paulfradgley <39525348+paulfradgley@users.noreply.github.com>
2022-06-14 14:32:12 +0100

    Fix formatting

--- test_conformance/c11_atomics/common.h
+++ test_conformance/c11_atomics/common.h
@@ -1358,13 +1358,12 @@ int CBasicTest<HostAtomicType, HostDataType>::ExecuteSingleTest(
         }
         if (LocalRefValues())
         {
-            error = clSetKernelArg(
-                kernel, argInd++,
-                LocalRefValues() ? typeSize
-                        * ((CurrentGroupSize() * NumNonAtomicVariablesPerThread())
-                           + 4)
-                                 : 1,
-                NULL);
+            error =
+                clSetKernelArg(kernel, argInd++,
+                               LocalRefValues() ? typeSize
+                                       * ((CurrentGroupSize()*NumNonAtomicVariablesPerThread()) + 4)
+                                                : 1,
+                               NULL);
             test_error(error, "Unable to set indexed kernel argument");
         }
     }
