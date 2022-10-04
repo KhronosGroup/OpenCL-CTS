d46df95fc0048c517785c34d13fe4f731f601926
Nikhil Joshi <nikhilj@nvidia.com>
2022-10-04 21:43:18 +0530

    Cap CL_DEVICE_MAX_MEM_ALLOC_SIZE to SIZE_MAX (#1501)
    * Fix enqueue_flags test to use correct barrier type.
    
    Currently, enqueue_flags test uses CLK_LOCAL_MEM_FENCE.
    Use CLK_GLOBAL_MEM_FENCE instead as all threads across work-groups
    need to wait here.
    
    * Add check for support for Read-Wrie images
    
    Read-Write images have required OpenCL 2.x.
    Read-Write image tests are already being skipped
    for 1.x devices.
    With OpenCL 3.0, read-write images being optional,
    the tests should be run or skipped
    depending on the implementation support.
    
    Add a check to decide if Read-Write images are
    supported or required to be supported depending
    on OpenCL version and decide if the tests should
    be run on skipped.
    
    Fixes issue #894
    
    * Fix formatting in case of Read-Write image checks.
    
    Fix formatting in case of Read-write image checks.
    Also, combine two ifs into one in case of
    kerne_read_write tests
    
    * Fix some more formatting for RW-image checks
    
    Remove unnecessary spaces at various places.
    Also, fix lengthy lines.
    
    * Fix malloc-size calculation in test imagedim
    
    unsigned char size is silently assumed to be 1
    in imagedim test of test_basic.
    Pass sizeof(type) in malloc size calculation.
    Also, change loop variable from signed to unsigned.
    Add checks for null pointer for malloced memory.
    
    * Cap CL_DEVICE_MAX_MEM_ALLOC_SIZE to SIZE_MAX
    
    Cap CL_DEVICE_MAX_MEM_ALLOC_SIZE to SIZE_MAX
    when CL_DEVICE_GLOBAL_MEM_SIZE is capped with SIZE_MAX.
    test_allocation caps the value of GLOBAL_MEM_SIZE to SIZE_MAX
    if it exceeds the value of SIZE_MAX(value depends on platform bitness),
    but doesn’t modify MAX_ALLOC_SIZE the same way.
    Due to this MAX_ALLOC_SIZE becomes greater than GLOBAL_MEM_SIZE
    and the test fails.
    
    Modify MAX_MEM_ALLOC_SIZE as GLOBAL_MEM_SIZE when it exceeds SIZE_MAX
    
    OpenCL-CTS #1022

--- test_conformance/images/clCopyImage/test_copy_3D.cpp
+++ test_conformance/images/clCopyImage/test_copy_3D.cpp
@@ -57,6 +57,7 @@ int test_copy_image_set_3D( cl_device_id device, cl_context context, cl_command_
 
     if (memSize > (cl_ulong)SIZE_MAX) {
       memSize = (cl_ulong)SIZE_MAX;
+      maxAllocSize = (cl_ulong)SIZE_MAX;
     }
 
     if( gTestSmallImages )
