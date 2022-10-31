b90f5b49d85603fe06896210bebc9b43f59fd462
John Kesapides <john.kesapides@arm.com>
2022-10-31 10:14:00 +0000

    Reformat using clang-format kernel source code.
    Signed-off-by: John Kesapides <john.kesapides@arm.com>

--- test_conformance/basic/test_barrier.cpp
+++ test_conformance/basic/test_barrier.cpp
@@ -29,28 +29,27 @@
 
 namespace {
 const char *barrier_kernel_code = R"(
-__kernel void compute_sum(__global int *a, int n, __global int *tmp_sum, __global int *sum)
+__kernel void compute_sum(__global int *a, int n, __global int *tmp_sum,
+                          __global int *sum)
 {
-    int  tid = get_local_id(0);
-    int  lsize = get_local_size(0);
-    int  i;
+    int tid = get_local_id(0);
+    int lsize = get_local_size(0);
+    int i;
 
     tmp_sum[tid] = 0;
-    for (i=tid; i<n; i+=lsize)
-        tmp_sum[tid] += a[i];
+    for (i = tid; i < n; i += lsize) tmp_sum[tid] += a[i];
 
     // updated to work for any workgroup size
-    for (i=hadd(lsize,1); lsize>1; i = hadd(i,1))
+    for (i = hadd(lsize, 1); lsize > 1; i = hadd(i, 1))
     {
         BARRIER(CLK_GLOBAL_MEM_FENCE);
-        if (tid + i < lsize)
-            tmp_sum[tid] += tmp_sum[tid + i];
-         lsize = i;
+        if (tid + i < lsize) tmp_sum[tid] += tmp_sum[tid + i];
+        lsize = i;
     }
 
-    //no barrier is required here because last person to write to tmp_sum[0] was tid 0
-    if (tid == 0)
-        *sum = tmp_sum[0];
+    // no barrier is required here because last person to write to tmp_sum[0]
+    // was tid 0
+    if (tid == 0) *sum = tmp_sum[0];
 }
 )";
 
