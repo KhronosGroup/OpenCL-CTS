9cc46e23ceb5feed0e18791322db6510bc25821b
Marcin Hajder <marcin.hajder@gmail.com>
2022-12-07 13:31:12 +0100

    Added simultaneous version of clSetKernelArg test, (#1369, p.1.7):
    https://github.com/KhronosGroup/OpenCL-CTS/issues/1369

--- test_conformance/extensions/cl_khr_command_buffer/command_buffer_set_kernel_arg.cpp
+++ test_conformance/extensions/cl_khr_command_buffer/command_buffer_set_kernel_arg.cpp
@@ -30,17 +30,10 @@ struct CommandBufferSetKernelArg : public BasicCommandBufferTest
 {
     CommandBufferSetKernelArg(cl_device_id device, cl_context context,
                               cl_command_queue queue)
-        : BasicCommandBufferTest(device, context, queue),
-          trigger_event(nullptr), wait_event(nullptr)
+        : BasicCommandBufferTest(device, context, queue), trigger_event(nullptr)
     {
         simultaneous_use_requested = simul_use;
-    }
-
-    //--------------------------------------------------------------------------
-    bool Skip() override
-    {
-        return (simultaneous_use_requested && !simultaneous_use_support)
-            || BasicCommandBufferTest::Skip();
+        if (simul_use) buffer_size_multiplier = 2;
     }
 
     //--------------------------------------------------------------------------
@@ -50,9 +43,11 @@ struct CommandBufferSetKernelArg : public BasicCommandBufferTest
 
         const char* kernel_str =
             R"(
-            __kernel void fill(int in, __global int* out) {
+            __kernel void copy(int in, __global int* out, __global int* offset)
+            {
                 size_t id = get_global_id(0);
-                out[id] = in;
+                size_t ind = offset[0] + id;
+                out[ind] = in;
             })";
 
         error = create_single_kernel_helper_create_program(context, &program, 1,
@@ -62,7 +57,7 @@ struct CommandBufferSetKernelArg : public BasicCommandBufferTest
         error = clBuildProgram(program, 1, &device, nullptr, nullptr, nullptr);
         test_error(error, "Failed to build program");
 
-        kernel = clCreateKernel(program, "fill", &error);
+        kernel = clCreateKernel(program, "copy", &error);
         test_error(error, "Failed to create print kernel");
 
         return CL_SUCCESS;
@@ -71,13 +66,26 @@ struct CommandBufferSetKernelArg : public BasicCommandBufferTest
     //--------------------------------------------------------------------------
     cl_int SetUpKernelArgs() override
     {
+        // do not extend buffers unless device support simultaneous use!
+        if (!simultaneous_use_support) buffer_size_multiplier = 1;
+
         cl_int error = CL_SUCCESS;
-        out_mem = clCreateBuffer(context, CL_MEM_WRITE_ONLY, data_size(),
+        out_mem = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
+                                 num_elements * buffer_size_multiplier
+                                     * sizeof(cl_int),
                                  nullptr, &error);
         test_error(error, "clCreateBuffer failed");
 
-        out_mem_k2 = clCreateBuffer(context, CL_MEM_WRITE_ONLY, data_size(),
+        out_mem_k2 = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
+                                    num_elements * buffer_size_multiplier
+                                        * sizeof(cl_int),
                                     nullptr, &error);
+        test_error(error, "clCreateBuffer failed");
+
+        cl_int offset = 0;
+        off_mem =
+            clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+                           sizeof(cl_int), &offset, &error);
         test_error(error, "clCreateBuffer failed");
 
         cl_int in_arg = pattern_pri;
@@ -87,6 +95,9 @@ struct CommandBufferSetKernelArg : public BasicCommandBufferTest
         error = clSetKernelArg(kernel, 1, sizeof(out_mem), &out_mem);
         test_error(error, "clSetKernelArg failed");
 
+        error = clSetKernelArg(kernel, 2, sizeof(off_mem), &off_mem);
+        test_error(error, "clSetKernelArg failed");
+
         return CL_SUCCESS;
     }
 
@@ -94,11 +105,6 @@ struct CommandBufferSetKernelArg : public BasicCommandBufferTest
     cl_int Run() override
     {
         cl_int error = CL_SUCCESS;
-
-        // record command buffer with primary queue
-        error = RecordCommandBuffer();
-        test_error(error, "RecordCommandBuffer failed");
-
         if (simultaneous_use_support)
         {
             // enqueue simultaneous command-buffers with printf calls
@@ -148,6 +154,10 @@ struct CommandBufferSetKernelArg : public BasicCommandBufferTest
         cl_int error = CL_SUCCESS;
         std::vector<cl_int> output_data(num_elements);
 
+        // record command buffer with primary queue
+        error = RecordCommandBuffer();
+        test_error(error, "RecordCommandBuffer failed");
+
         const cl_int pattern_base = 0;
         error =
             clEnqueueFillBuffer(queue, out_mem, &pattern_base, sizeof(cl_int),
@@ -177,21 +187,120 @@ struct CommandBufferSetKernelArg : public BasicCommandBufferTest
             CHECK_VERIFICATION_ERROR(pattern_pri, output_data[i], i);
         }
 
+        return CL_SUCCESS;
+    }
+
+    //--------------------------------------------------------------------------
+    struct SimulPassData
+    {
+        cl_int pattern;
+        cl_int offset;
+        std::vector<cl_int> output_buffer;
+    };
+
+    //--------------------------------------------------------------------------
+    cl_int RecordSimultaneousCommandBuffer() const
+    {
+        cl_int error = CL_SUCCESS;
+
+        error = clCommandNDRangeKernelKHR(
+            command_buffer, nullptr, nullptr, kernel, 1, nullptr, &num_elements,
+            nullptr, 0, nullptr, nullptr, nullptr);
+        test_error(error, "clCommandNDRangeKernelKHR failed");
+
+        error = clFinalizeCommandBufferKHR(command_buffer);
+        test_error(error, "clFinalizeCommandBufferKHR failed");
+        return CL_SUCCESS;
+    }
+
+    //--------------------------------------------------------------------------
+    cl_int EnqueueSimultaneousPass(SimulPassData& pd)
+    {
+        cl_int error = clEnqueueFillBuffer(
+            queue, out_mem, &pd.pattern, sizeof(cl_int),
+            pd.offset * sizeof(cl_int), data_size(), 0, nullptr, nullptr);
+        test_error(error, "clEnqueueFillBuffer failed");
+
+        error = clEnqueueFillBuffer(queue, off_mem, &pd.offset, sizeof(cl_int),
+                                    0, sizeof(cl_int), 0, nullptr, nullptr);
+        test_error(error, "clEnqueueFillBuffer failed");
+
+        if (!trigger_event)
+        {
+            trigger_event = clCreateUserEvent(context, &error);
+            test_error(error, "clCreateUserEvent failed");
+        }
+
+        error = clEnqueueCommandBufferKHR(0, nullptr, command_buffer, 1,
+                                          &trigger_event, nullptr);
+        test_error(error, "clEnqueueCommandBufferKHR failed");
+
+        error = clEnqueueReadBuffer(
+            queue, out_mem, CL_FALSE, pd.offset * sizeof(cl_int), data_size(),
+            pd.output_buffer.data(), 0, nullptr, nullptr);
+        test_error(error, "clEnqueueReadBuffer failed");
 
         return CL_SUCCESS;
     }
 
     //--------------------------------------------------------------------------
-    cl_int RunSimultaneous() { return CL_SUCCESS; }
+    cl_int RunSimultaneous()
+    {
+        cl_int error = CL_SUCCESS;
+
+        // record command buffer with primary queue
+        error = RecordSimultaneousCommandBuffer();
+        test_error(error, "RecordCommandBuffer failed");
+
+        std::vector<SimulPassData> simul_passes = {
+            { 0, 0, std::vector<cl_int>(num_elements) }
+        };
+
+        error = EnqueueSimultaneousPass(simul_passes.front());
+        test_error(error, "EnqueueSimultaneousPass 1 failed");
+
+        cl_int in_arg = pattern_sec;
+        error = clSetKernelArg(kernel, 0, sizeof(cl_int), &in_arg);
+        test_error(error, "clSetKernelArg failed");
+
+        error = clSetKernelArg(kernel, 1, sizeof(out_mem_k2), &out_mem_k2);
+        test_error(error, "clSetKernelArg failed");
+
+        if (simultaneous_use_support)
+        {
+            cl_int offset = static_cast<cl_int>(num_elements);
+            simul_passes.push_back(
+                { 1, offset, std::vector<cl_int>(num_elements) });
+
+            error = EnqueueSimultaneousPass(simul_passes.back());
+            test_error(error, "EnqueueSimultaneousPass 2 failed");
+        }
+
+        error = clSetUserEventStatus(trigger_event, CL_COMPLETE);
+        test_error(error, "clSetUserEventStatus failed");
+
+        error = clFinish(queue);
+        test_error(error, "clFinish failed");
+
+        for (auto&& pass : simul_passes)
+        {
+            auto& res_data = pass.output_buffer;
+            for (size_t i = 0; i < num_elements; i++)
+            {
+                CHECK_VERIFICATION_ERROR(pattern_pri, res_data[i], i);
+            }
+        }
+
+        return CL_SUCCESS;
+    }
 
     //--------------------------------------------------------------------------
     clEventWrapper trigger_event = nullptr;
-    clEventWrapper wait_event = nullptr;
 
     const cl_int pattern_pri = 2;
     const cl_int pattern_sec = 3;
 
-    clMemWrapper out_mem_k2;
+    clMemWrapper out_mem_k2 = nullptr;
 };
 
 } // anonymous namespace
