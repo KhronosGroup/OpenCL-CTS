c1e02c38ba13f5a8f500607e89ec3a54245c11f7
Kévin Petit <kpet@free.fr>
2023-01-14 15:18:27 +0000

    Get rid of threadTesting.h (#1604)
    It only contains a pointer type definition for test functions that
    really ought to be provided by testHarness.h.
    
    Signed-off-by: Kévin Petit <kpet@free.fr>
    
    Signed-off-by: Kévin Petit <kpet@free.fr>

--- test_conformance/atomics/procs.h
+++ test_conformance/atomics/procs.h
@@ -15,7 +15,6 @@
 //
 #include "harness/errorHelpers.h"
 #include "harness/kernelHelpers.h"
-#include "harness/threadTesting.h"
 #include "harness/typeWrappers.h"
 
 extern int create_program_and_kernel(const char *source,
