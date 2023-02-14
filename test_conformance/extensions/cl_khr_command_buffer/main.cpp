7fecd1f5a56763d23f8dd5d99ee08973103f67ae
Paweł Jastrzębski <p.k.jastrzebski@gmail.com>
2023-02-14 11:47:05 +0100

    Superficial fixes for review.
    * Fix initialisation order of out_of_order_command_buffer and out_of_order_queue for BasicCommandBufferTest
    * Move variables definitions into Run() function
    * Add prefixes for Event Info tests
    * Return TEST_FAIL instead of -1
    
    Signed-off-by: Paweł Jastrzębski <p.k.jastrzebski@gmail.com>

--- test_conformance/extensions/cl_khr_command_buffer/main.cpp
+++ test_conformance/extensions/cl_khr_command_buffer/main.cpp
@@ -32,11 +32,11 @@ test_definition test_list[] = { ADD_TEST(single_ndrange),
                                 ADD_TEST(copy_image_to_buffer),
                                 ADD_TEST(copy_buffer_rect),
                                 ADD_TEST(barrier_wait_list),
-                                ADD_TEST(command_type),
-                                ADD_TEST(command_queue),
-                                ADD_TEST(execution_status),
-                                ADD_TEST(context),
-                                ADD_TEST(reference_count) };
+                                ADD_TEST(event_info_command_type),
+                                ADD_TEST(event_info_command_queue),
+                                ADD_TEST(event_info_execution_status),
+                                ADD_TEST(event_info_context),
+                                ADD_TEST(event_info_reference_count) };
 
 
 int main(int argc, const char *argv[])
