475a37abbfa22a55fe47bf76d5c7904b3a37730a
Sven van Haastregt <sven.vanhaastregt@arm.com>
2023-06-09 11:25:20 +0100

    [NFC] Do not use reserved names for include guards (#1737)
    Names that begin with an underscore followed by an uppercase letter
    are reserved for the C++ implementation.
    
    Signed-off-by: Sven van Haastregt <sven.vanhaastregt@arm.com>

--- test_conformance/extensions/cl_khr_command_buffer/cl_khr_command_buffer_mutable_dispatch/procs.h
+++ test_conformance/extensions/cl_khr_command_buffer/cl_khr_command_buffer_mutable_dispatch/procs.h
@@ -13,8 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-#ifndef _CL_KHR_COMMAND_BUFFER_MUTABLE_DISPATCH_PROCS_H
-#define _CL_KHR_COMMAND_BUFFER_MUTABLE_DISPATCH_PROCS_H
+#ifndef CL_KHR_COMMAND_BUFFER_MUTABLE_DISPATCH_PROCS_H
+#define CL_KHR_COMMAND_BUFFER_MUTABLE_DISPATCH_PROCS_H
 
 #include <CL/cl.h>
 
@@ -59,4 +59,4 @@ extern int test_mutable_command_info_global_work_size(cl_device_id device,
                                                       cl_context context,
                                                       cl_command_queue queue,
                                                       int num_elements);
-#endif /*_CL_KHR_COMMAND_BUFFER_MUTABLE_DISPATCH_PROCS_H*/
+#endif // CL_KHR_COMMAND_BUFFER_MUTABLE_DISPATCH_PROCS_H
