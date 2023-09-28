d705c5c324163e1337072ca514e5409e60326c4a
Michal Babej <michal.babej@intel.com>
2023-09-28 13:51:48 +0300

    add Khronos license + minor fixes

--- test_conformance/extensions/cl_khr_command_buffer/svm_command_basic.cpp
+++ test_conformance/extensions/cl_khr_command_buffer/svm_command_basic.cpp
@@ -1,6 +1,19 @@
+//
+// Copyright (c) 2023 The Khronos Group Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
-#include "basic_command_buffer.h"
-#include "command_buffer_test_base.h"
 #include "svm_command_basic.h"
 
 //--------------------------------------------------------------------------
@@ -17,7 +30,7 @@ bool BasicSVMCommandBufferTest::Skip()
     }
 
     cl_device_svm_capabilities svm_capabilities;
-    int error =
+    cl_int error =
         clGetDeviceInfo(device, CL_DEVICE_SVM_CAPABILITIES,
                         sizeof(svm_capabilities), &svm_capabilities, NULL);
     if (error != CL_SUCCESS)
--- test_conformance/extensions/cl_khr_command_buffer/svm_command_basic.h
+++ test_conformance/extensions/cl_khr_command_buffer/svm_command_basic.h
@@ -1,9 +1,23 @@
+//
+// Copyright (c) 2023 The Khronos Group Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
 #ifndef CL_KHR_SVM_COMMAND_BASIC_H
 #define CL_KHR_SVM_COMMAND_BASIC_H
 
 #include "basic_command_buffer.h"
-#include "command_buffer_test_base.h"
 
 
 struct BasicSVMCommandBufferTest : BasicCommandBufferTest
