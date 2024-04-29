KhronosGroup/OpenCL-CTS

commit 77293c83bd4080d3f1065fa4fe6115595e222f86

musl-libc doesn't provide GNU version of strerror_r but uses BSD/XSI one (#1911)
* harness: correct include and sort

warning redirecting incorrect #include <sys/errno.h> to <errno.h>

Signed-off-by: David Heidelberg <david@ixit.cz>

test_common/harness/os_helpers.cpp
@@ -208,7 +208,8 @@ static std::string _err_msg(int err, int level)
     */
 
 #if (defined(__ANDROID__) && __ANDROID_API__ < 23)                             \
-    || ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !_GNU_SOURCE)
+    || ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !_GNU_SOURCE)  \
+    || (defined(_GNU_SOURCE) && !defined(__GLIBC__))
 
 // XSI version of strerror_r.
 #warning Not tested!

test_common/harness/ThreadPool.cpp
@@ -33,9 +33,9 @@
 #include "mingw_compat.h"
 #include <process.h>
 #else // !_WIN32
+#include <errno.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <sys/errno.h>
 #ifdef __linux__
 #include <sched.h>
 #endif

commit 699007ff431b4c2c6308149dcd5d3a8bb7ebea5c

Fix build breakage introduced by #1911

test_common/harness/os_helpers.cpp
@@ -209,7 +209,7 @@ static std::string _err_msg(int err, int level)
 
 #if (defined(__ANDROID__) && __ANDROID_API__ < 23)                             \
     || ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !_GNU_SOURCE)  \
-    || (defined(_GNU_SOURCE) && !defined(__GLIBC__))
+    || (defined(_GNU_SOURCE) && !defined(__GLIBC__) && !defined(__USE_GNU))
 
 // XSI version of strerror_r.
 #warning Not tested!