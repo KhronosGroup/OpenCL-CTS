5093ce5be558967c038794e2a61081430b1fc9f6
Kamil-Goras-Mobica <141216953+kamil-goras-mobica@users.noreply.github.com>
2024-05-14 17:42:46 +0200

    Added negative test commandNDRangeKernel (#1936)
    According to description
    https://github.com/KhronosGroup/OpenCL-CTS/issues/1668

--- test_conformance/extensions/cl_khr_command_buffer/procs.h
+++ test_conformance/extensions/cl_khr_command_buffer/procs.h
@@ -172,6 +172,34 @@ extern int
 test_negative_create_command_buffer_device_does_not_support_out_of_order_queue(
     cl_device_id device, cl_context context, cl_command_queue queue,
     int num_elements);
+extern int test_negative_command_ndrange_queue_not_null(cl_device_id device,
+                                                        cl_context context,
+                                                        cl_command_queue queue,
+                                                        int num_elements);
+extern int test_negative_command_ndrange_kernel_with_different_context(
+    cl_device_id device, cl_context context, cl_command_queue queue,
+    int num_elements);
+extern int test_negative_command_ndrange_kernel_sync_points_null_or_num_zero(
+    cl_device_id device, cl_context context, cl_command_queue queue,
+    int num_elements);
+extern int test_negative_command_ndrange_kernel_invalid_command_buffer(
+    cl_device_id device, cl_context context, cl_command_queue queue,
+    int num_elements);
+extern int test_negative_command_ndrange_kernel_invalid_properties(
+    cl_device_id device, cl_context context, cl_command_queue queue,
+    int num_elements);
+extern int test_negative_command_ndrange_kernel_command_buffer_finalized(
+    cl_device_id device, cl_context context, cl_command_queue queue,
+    int num_elements);
+extern int test_negative_command_ndrange_kernel_mutable_handle_not_null(
+    cl_device_id device, cl_context context, cl_command_queue queue,
+    int num_elements);
+extern int test_negative_command_ndrange_kernel_not_support_printf(
+    cl_device_id device, cl_context context, cl_command_queue queue,
+    int num_elements);
+extern int test_negative_command_ndrange_kernel_with_enqueue_call(
+    cl_device_id device, cl_context context, cl_command_queue queue,
+    int num_elements);
 extern int test_negative_get_command_buffer_info_invalid_command_buffer(
     cl_device_id device, cl_context context, cl_command_queue queue,
     int num_elements);
