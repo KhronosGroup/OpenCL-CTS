d3e3bdabaa0008810c7b5dcc832cf2e028c4b36c
Kamil-Goras-Mobica <141216953+kamil-goras-mobica@users.noreply.github.com>
2024-05-22 10:18:36 +0200

    Negative tests clCommandSvmMem[cpy, Fill]KHR (#1956)
    According to description
    https://github.com/KhronosGroup/OpenCL-CTS/issues/1668

--- test_conformance/extensions/cl_khr_command_buffer/procs.h
+++ test_conformance/extensions/cl_khr_command_buffer/procs.h
@@ -224,6 +224,22 @@ extern int test_negative_get_command_buffer_info_context(cl_device_id device,
                                                          cl_context context,
                                                          cl_command_queue queue,
                                                          int num_elements);
+extern int test_negative_command_buffer_command_svm_queue_not_null(
+    cl_device_id device, cl_context context, cl_command_queue queue,
+    int num_elements);
+extern int
+test_negative_command_buffer_command_svm_sync_points_null_or_num_zero(
+    cl_device_id device, cl_context context, cl_command_queue queue,
+    int num_elements);
+extern int test_negative_command_buffer_command_svm_invalid_command_buffer(
+    cl_device_id device, cl_context context, cl_command_queue queue,
+    int num_elements);
+extern int test_negative_command_buffer_command_svm_finalized_command_buffer(
+    cl_device_id device, cl_context context, cl_command_queue queue,
+    int num_elements);
+extern int test_negative_command_buffer_command_svm_mutable_handle_not_null(
+    cl_device_id device, cl_context context, cl_command_queue queue,
+    int num_elements);
 extern int test_negative_command_buffer_barrier_not_null_queue(
     cl_device_id device, cl_context context, cl_command_queue queue,
     int num_elements);
