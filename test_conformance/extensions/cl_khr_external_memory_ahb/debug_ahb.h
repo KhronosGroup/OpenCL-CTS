acae72f3eba470115e3176e259942b7d1980778f
dcrawleyqti <quic_dcrawley@quicinc.com>
2024-08-18 13:33:26 -0700

    cl_khr_external_memory_android_hardware_buffer
    Android AHardwareBuffer test. Initial testing
    for buffers and images.

--- test_conformance/extensions/cl_khr_external_memory_ahb/debug_ahb.h
+++ test_conformance/extensions/cl_khr_external_memory_ahb/debug_ahb.h
@@ -5,5 +5,3 @@
 const char * ahardwareBufferFormatToString(AHardwareBuffer_Format format);
 const char * ahardwareBufferUsageFlagToString(AHardwareBuffer_UsageFlags flag);
 char * ahardwareBufferDecodeUsageFlagsToString(AHardwareBuffer_UsageFlags flags);
-
-#define CL_EXTERNAL_MEMORY_HANDLE_AHB_KHR  0x41A5
