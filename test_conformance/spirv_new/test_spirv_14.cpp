8732c76b9ae37eb4d9b042ae871d9b966b7357c8
Ben Ashbaugh <ben.ashbaugh@intel.com>
2024-11-02 16:53:08 -0700

    switch to the kernel that uses OpCopyLogical

--- test_conformance/spirv_new/test_spirv_14.cpp
+++ test_conformance/spirv_new/test_spirv_14.cpp
@@ -360,19 +360,16 @@ TEST_SPIRV_FUNC(spirv14_copylogical)
         log_info("SPIR-V 1.4 not supported; skipping tests.\n");
         return TEST_SKIPPED_ITSELF;
     }
+
     cl_int error = CL_SUCCESS;
     clProgramWrapper prog;
-#if 0
     error = get_program_with_il(prog, deviceID, context,
                                 "spv1.4/copylogical_struct");
-#else
-    // !!! TODO: Delete the copyobject file also, when this code is removed!
-    error = get_program_with_il(prog, deviceID, context,
-                                "spv1.4/copyobject_struct");
-#endif
     SPIRV_CHECK_ERROR(error, "Failed to compile spv program");
+
     clKernelWrapper kernel = clCreateKernel(prog, "copylogical_test", &error);
     SPIRV_CHECK_ERROR(error, "Failed to create spv kernel");
+
     struct TestStruct
     {
         cl_int i;
@@ -382,15 +379,19 @@ TEST_SPIRV_FUNC(spirv14_copylogical)
     clMemWrapper dst = clCreateBuffer(context, CL_MEM_READ_WRITE,
                                       sizeof(results), NULL, &error);
     SPIRV_CHECK_ERROR(error, "Failed to create dst buffer");
+
     error |= clSetKernelArg(kernel, 0, sizeof(dst), &dst);
     SPIRV_CHECK_ERROR(error, "Failed to set kernel args");
+
     size_t global = 1;
     error = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global, NULL, 0,
                                    NULL, NULL);
     SPIRV_CHECK_ERROR(error, "Failed to enqueue kernel");
+
     error = clEnqueueReadBuffer(queue, dst, CL_TRUE, 0, sizeof(results),
                                 &results, 0, NULL, NULL);
     SPIRV_CHECK_ERROR(error, "Unable to read destination buffer");
+
     if (results.i != 1024 || results.f != 3.1415f)
     {
         log_error(
@@ -398,5 +399,6 @@ TEST_SPIRV_FUNC(spirv14_copylogical)
             results.i, results.f);
         return TEST_FAIL;
     }
+
     return TEST_PASS;
 }
