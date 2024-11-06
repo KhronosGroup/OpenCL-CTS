7484f2d31a90b92539dc097135dc085889676c04
Michal Babej <90404+franz@users.noreply.github.com>
2024-11-06 20:17:15 +0200

    mutable_command_info: fix Skip logic in PropertiesArray test
    the test uses CL_MUTABLE_DISPATCH_ARGUMENTS_KHR,
    but does not check for support.

--- test_conformance/extensions/cl_khr_command_buffer/cl_khr_command_buffer_mutable_dispatch/mutable_command_info.cpp
+++ test_conformance/extensions/cl_khr_command_buffer/cl_khr_command_buffer_mutable_dispatch/mutable_command_info.cpp
@@ -124,7 +124,6 @@ struct PropertiesArray : public InfoMutableCommandBufferTest
         if ((device_version >= Version(3, 0))
             || is_extension_available(device, "cl_khr_extended_versioning"))
         {
-
             cl_version extension_version = get_extension_version(
                 device, "cl_khr_command_buffer_mutable_dispatch");
 
@@ -133,6 +132,16 @@ struct PropertiesArray : public InfoMutableCommandBufferTest
                 return true;
             }
         }
+
+        cl_mutable_dispatch_fields_khr mutable_capabilities;
+        cl_int error = clGetDeviceInfo(
+            device, CL_DEVICE_MUTABLE_DISPATCH_CAPABILITIES_KHR,
+            sizeof(mutable_capabilities), &mutable_capabilities, nullptr);
+        test_error(error, "clGetDeviceInfo failed");
+
+        if ((mutable_capabilities & CL_MUTABLE_DISPATCH_ARGUMENTS_KHR) == 0)
+            return true;
+
         return false;
     }
 
