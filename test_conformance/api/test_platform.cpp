ec8e5600978822b8c2befdb615b4147ecd163874
Ben Ashbaugh <ben.ashbaugh@intel.com>
2024-12-12 17:25:12 -0800

    modernize tokenization using istringstream

--- test_conformance/api/test_platform.cpp
+++ test_conformance/api/test_platform.cpp
@@ -18,6 +18,7 @@
 #include <string.h>
 
 #include <algorithm>
+#include <sstream>
 
 int test_platform_extensions(cl_device_id deviceID, cl_context context,
                              cl_command_queue queue, int num_elements)
@@ -38,73 +39,63 @@ int test_platform_extensions(cl_device_id deviceID, cl_context context,
                           (void *)(&platformID), NULL);
     test_error(err, "clGetDeviceInfo failed for CL_DEVICE_PLATFORM");
 
-    // Note that we read into a vector for tokenization, rather than a string,
-    // because strtok_r modifies the string.
-
-    size_t size = 0;
-
-    err = clGetPlatformInfo(platformID, CL_PLATFORM_EXTENSIONS, 0, NULL, &size);
-    test_error(err,
-               "clGetPlatformInfo failed to get CL_PLATFORM_EXTENSIONS size");
-
-    std::vector<char> platformExtensions(size);
-    err = clGetPlatformInfo(platformID, CL_PLATFORM_EXTENSIONS, size,
-                            platformExtensions.data(), NULL);
-    test_error(err, "clGetPlatformInfo failed for CL_PLATFORM_EXTENSIONS");
-
-    err = clGetDeviceInfo(deviceID, CL_DEVICE_EXTENSIONS, 0, NULL, &size);
-    test_error(err, "clGetDeviceInfo failed to get CL_DEVICE_EXTENSIONS size");
-
-    std::vector<char> deviceExtensions(size);
-    err = clGetDeviceInfo(deviceID, CL_DEVICE_EXTENSIONS, size,
-                          deviceExtensions.data(), NULL);
-    test_error(err, "clGetDeviceInfo failed for CL_DEVICE_EXTENSIONS");
-
     // First, check that all platform extensions are reported by the device.
-
-    for (char *saveptr = nullptr,
-              *token = strtok_r(platformExtensions.data(), " ", &saveptr);
-         token != nullptr; token = strtok_r(nullptr, " ", &saveptr))
     {
-        // log_info("Checking platform extension: %s\n", token);
-        bool isPlatformExtension = std::find(cPlatformExtensions.begin(),
-                                             cPlatformExtensions.end(), token)
-            != cPlatformExtensions.end();
-        if (!isPlatformExtension && !is_extension_available(deviceID, token))
+        std::string platformExtensions =
+            get_platform_info_string(platformID, CL_PLATFORM_EXTENSIONS);
+
+        std::istringstream is(platformExtensions);
+        std::string token;
+        while (std::getline(is, token, ' '))
         {
-            test_fail("%s is not a platform extension and is supported by the "
-                      "platform but not by the device\n",
-                      token);
+            //log_info("Checking platform extension: %s\n", token.c_str());
+            bool isPlatformExtension =
+                std::find(cPlatformExtensions.begin(),
+                          cPlatformExtensions.end(), token)
+                != cPlatformExtensions.end();
+            if (!isPlatformExtension
+                && !is_extension_available(deviceID, token.c_str()))
+            {
+                test_fail(
+                    "%s is not a platform extension and is supported by the "
+                    "platform but not by the device\n",
+                    token.c_str());
+            }
         }
     }
 
     // Next, check that device extensions reported by all devices are reported
     // by the platform.
-
-    cl_uint numDevices = 0;
-    err = clGetDeviceIDs(platformID, CL_DEVICE_TYPE_ALL, 0, NULL, &numDevices);
-    test_error(err, "clGetDeviceIDs failed to get number of devices");
-
-    std::vector<cl_device_id> devices(numDevices);
-    err = clGetDeviceIDs(platformID, CL_DEVICE_TYPE_ALL, numDevices,
-                         devices.data(), NULL);
-    test_error(err, "clGetDeviceIDs failed to get device IDs");
-
-    for (char *saveptr = nullptr,
-              *token = strtok_r(deviceExtensions.data(), " ", &saveptr);
-         token != nullptr; token = strtok_r(nullptr, " ", &saveptr))
     {
-        // log_info("Checking device extension: %s\n", token);
-        bool supportedByAllDevices = std::all_of(
-            devices.begin(), devices.end(), [&](cl_device_id device) {
-                return is_extension_available(device, token);
-            });
-        if (supportedByAllDevices
-            && !is_platform_extension_available(platformID, token))
+        cl_uint numDevices = 0;
+        err = clGetDeviceIDs(platformID, CL_DEVICE_TYPE_ALL, 0, NULL,
+                             &numDevices);
+        test_error(err, "clGetDeviceIDs failed to get number of devices");
+
+        std::vector<cl_device_id> devices(numDevices);
+        err = clGetDeviceIDs(platformID, CL_DEVICE_TYPE_ALL, numDevices,
+                             devices.data(), NULL);
+        test_error(err, "clGetDeviceIDs failed to get device IDs");
+
+        std::string deviceExtensions =
+            get_device_info_string(deviceID, CL_DEVICE_EXTENSIONS);
+
+        std::istringstream is(deviceExtensions);
+        std::string token;
+        while (std::getline(is, token, ' '))
         {
-            test_fail(
-                "%s is supported by all devices but not by the platform\n",
-                token);
+            //log_info("Checking device extension: %s\n", token.c_str());
+            bool supportedByAllDevices = std::all_of(
+                devices.begin(), devices.end(), [&](cl_device_id device) {
+                    return is_extension_available(device, token.c_str());
+                });
+            if (supportedByAllDevices
+                && !is_platform_extension_available(platformID, token.c_str()))
+            {
+                test_fail(
+                    "%s is supported by all devices but not by the platform\n",
+                    token.c_str());
+            }
         }
     }
 
