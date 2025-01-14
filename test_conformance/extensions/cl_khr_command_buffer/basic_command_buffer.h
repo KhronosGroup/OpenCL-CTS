98d52d0d5663a5b9244beb50388792d921c0562d
Ewan Crawford <ewan@codeplay.com>
2025-01-14 17:06:30 +0000

    Check command-buffer extension version for equality (#2215)
    As described in Issue
    https://github.com/KhronosGroup/OpenCL-CTS/issues/2152 we currently
    check the provisional extension version supported by a vendor is the
    same or older than a test specified version. However, it was discussed
    in the WG that this should be a check for equality to avoid hitting
    issues when a implementation tests against an older version of the CTS
    using a lower extension version, with API breaking changes having
    occurred since.
    
    The tests for the command-buffer family of extensions are the only
    provisional KHR tests using this versioning check, so this PR updates
    all those cases to equality.

--- test_conformance/extensions/cl_khr_command_buffer/basic_command_buffer.h
+++ test_conformance/extensions/cl_khr_command_buffer/basic_command_buffer.h
@@ -104,11 +104,10 @@ int MakeAndRunTest(cl_device_id device, cl_context context,
         cl_version extension_version =
             get_extension_version(device, "cl_khr_command_buffer");
 
-        if (extension_version < CL_MAKE_VERSION(0, 9, 5))
+        if (extension_version != CL_MAKE_VERSION(0, 9, 5))
         {
 
-            log_info("cl_khr_command_buffer version 0.9.5 or later is required "
-                     "to run "
+            log_info("cl_khr_command_buffer version 0.9.5 is required to run "
                      "the test, skipping.\n ");
             return TEST_SKIPPED_ITSELF;
         }
