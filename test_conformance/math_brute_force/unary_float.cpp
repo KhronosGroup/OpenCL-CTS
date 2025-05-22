39d3b6bcae5ef18ab8beb398291c1eef628e6366
Ben Ashbaugh <ben.ashbaugh@intel.com>
2025-05-22 15:31:05 -0700

    only add correctly rounded build option for sqrt_cr and divide_cr
    remove option from usage text

--- test_conformance/math_brute_force/unary_float.cpp
+++ test_conformance/math_brute_force/unary_float.cpp
@@ -563,10 +563,12 @@ int TestFunc_Float_Float(const Func *f, MTdata d, bool relaxedMode)
             INFINITY; // out of range resut from finite inputs must be numeric
     }
 
+    bool correctlyRounded = strcmp(f->name, "sqrt_cr") == 0;
+
     // Init the kernels
     BuildKernelInfo build_info{ test_info.threadCount, test_info.k,
                                 test_info.programs, f->nameInCode,
-                                relaxedMode };
+                                relaxedMode, correctlyRounded };
     if ((error = ThreadPool_Do(BuildKernelFn,
                                gMaxVectorSizeIndex - gMinVectorSizeIndex,
                                &build_info)))
