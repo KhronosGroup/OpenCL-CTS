25acb51f8d0b687e37d267881c0d9e18edbce482
Ben Ashbaugh <ben.ashbaugh@intel.com>
2025-07-03 15:26:40 -0700

    add a few more zero-sized SVM tests
    clSetKernelExecInfo with an empty set
    clEnqueueSVMFree with an empty set

--- test_conformance/SVM/test_size_zero.cpp
+++ test_conformance/SVM/test_size_zero.cpp
@@ -16,7 +16,7 @@
 #include "common.h"
 
 static int svm_size_zero_helper(cl_device_id device, cl_context context,
-                                cl_command_queue queue,
+                                cl_command_queue queue, cl_kernel kernel,
                                 cl_svm_mem_flags svmFlags)
 {
     cl_int error = CL_SUCCESS;
@@ -46,7 +46,8 @@ static int svm_size_zero_helper(cl_device_id device, cl_context context,
     error = clEnqueueSVMMemFill(queue, svmPtr(), &value, sizeof(value), 0, 0,
                                 nullptr, &event);
     // test_error(error, "clEnqueueSVMMemFill with zero size failed");
-    log_info("      clEnqueueSVMMemFill with zero size returned %d\n", error);
+    log_info("      clEnqueueSVMMemFill with zero size returned %s\n",
+             IGetErrorString(error));
 
     // Check that the event for the zero-sized fill has the right command type
     if (error == CL_SUCCESS)
@@ -69,8 +70,8 @@ static int svm_size_zero_helper(cl_device_id device, cl_context context,
     error = clEnqueueSVMMemcpy(queue, CL_TRUE, svmPtr(), &value, 0, 0, nullptr,
                                &event);
     // test_error(error, "clEnqueueSVMMemcpy to SVM with zero size failed");
-    log_info("      clEnqueueSVMMemcpy to SVM with zero size returned %d\n",
-             error);
+    log_info("      clEnqueueSVMMemcpy to SVM with zero size returned %s\n",
+             IGetErrorString(error));
 
     // Check that the event for the zero-sized copy has the right command type
     if (error == CL_SUCCESS)
@@ -91,8 +92,8 @@ static int svm_size_zero_helper(cl_device_id device, cl_context context,
     error = clEnqueueSVMMemcpy(queue, CL_TRUE, &value, svmPtr(), 0, 0, nullptr,
                                &event);
     // test_error(error, "clEnqueueSVMMemcpy from SVM with zero size failed");
-    log_info("      clEnqueueSVMMemcpy from SVM with zero size returned %d\n",
-             error);
+    log_info("      clEnqueueSVMMemcpy from SVM with zero size returned %s\n",
+             IGetErrorString(error));
 
     // Check that the event for the zero-sized copy has the right command type
     if (error == CL_SUCCESS)
@@ -116,8 +117,8 @@ static int svm_size_zero_helper(cl_device_id device, cl_context context,
                                    CL_MIGRATE_MEM_OBJECT_CONTENT_UNDEFINED, 0,
                                    nullptr, &event);
     // test_error(error, "clEnqueueSVMMigrateMem with zero size failed");
-    log_info("      clEnqueueSVMMigrateMem with zero size returned %d\n",
-             error);
+    log_info("      clEnqueueSVMMigrateMem with zero size returned %s\n",
+             IGetErrorString(error));
 
     // Check that the event for the zero-sized migration has the right command
     // type
@@ -134,6 +135,33 @@ static int svm_size_zero_helper(cl_device_id device, cl_context context,
                  cmdType, CL_COMMAND_SVM_MIGRATE_MEM);
     }
 
+    // Try to call clSetKernelExecInfo with an empty set
+    error =
+        clSetKernelExecInfo(kernel, CL_KERNEL_EXEC_INFO_SVM_PTRS, 0, nullptr);
+    // test_error(error, "clSetKernelExecInfo with an empty set failed");
+    log_info("      clSetKernelExecInfo with an empty set returned %s\n",
+             IGetErrorString(error));
+
+    // Try to call clEnqueueSVMFree with an empty set
+    event = nullptr; // Reset the event
+    error = clEnqueueSVMFree(queue, 0, nullptr, nullptr, nullptr, 0, nullptr,
+                             &event);
+    // test_error(error, "clEnqueueSVMFree with an empty set failed");
+    log_info("      clEnqueueSVMFree with an empty set returned %s\n",
+             IGetErrorString(error));
+
+    if (error == CL_SUCCESS)
+    {
+        error = clGetEventInfo(event, CL_EVENT_COMMAND_TYPE, sizeof(cmdType),
+                               &cmdType, nullptr);
+        test_error(error, "clGetEventInfo failed for CL_EVENT_COMMAND_TYPE");
+        // test_assert_error(cmdType == CL_COMMAND_SVM_FREE,
+        //                   "Unexpected command type for clEnqueueSVMMigrateMem
+        //                   " "with zero size");
+        log_info("      clEnqueueSVMFree with zero size has command type "
+                 "%4X (%4X)\n",
+                 cmdType, CL_COMMAND_SVM_FREE);
+    }
     return TEST_PASS;
 }
 
@@ -166,23 +194,32 @@ REGISTER_TEST(svm_size_zero)
                             &svmCaps, nullptr);
     test_error(error, "clGetDeviceInfo failed to get SVM capabilities");
 
+    clProgramWrapper program;
+    clKernelWrapper kernel;
+    const char* programString = R"(
+        kernel void test_OneArg(global int* ptr) { ptr[0] = 0; }
+    )";
+    error = create_single_kernel_helper(context, &program, &kernel, 1,
+                                        &programString, "test_OneArg");
+    test_error(error, "could not create test kernel");
+
     int result = TEST_PASS;
 
     if (svmCaps & CL_DEVICE_SVM_COARSE_GRAIN_BUFFER)
     {
         log_info("    testing coarse-grain SVM\n");
-        result |= svm_size_zero_helper(device, context, queue, 0);
+        result |= svm_size_zero_helper(device, context, queue, kernel, 0);
     }
     if (svmCaps & CL_DEVICE_SVM_FINE_GRAIN_BUFFER)
     {
         log_info("    testing fine-grain SVM\n");
-        result |= svm_size_zero_helper(device, context, queue,
+        result |= svm_size_zero_helper(device, context, queue, kernel,
                                        CL_MEM_SVM_FINE_GRAIN_BUFFER);
     }
     if (svmCaps & (CL_DEVICE_SVM_FINE_GRAIN_BUFFER | CL_DEVICE_SVM_ATOMICS))
     {
         log_info("    testing fine-grain SVM with atomics\n");
-        result |= svm_size_zero_helper(device, context, queue,
+        result |= svm_size_zero_helper(device, context, queue, kernel,
                                        CL_MEM_SVM_FINE_GRAIN_BUFFER
                                            | CL_MEM_SVM_ATOMICS);
     }
