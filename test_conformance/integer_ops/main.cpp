037f6d181337ed7bcc50c67f93df8823e3d4c273
Ahmed Hesham <ahmed.hesham@arm.com>
2025-07-30 18:10:11 +0100

    Apply clang-format
    Applying formatting changes in a separate commit to ease code
    reviewing.
    
    Signed-off-by: Ahmed Hesham <ahmed.hesham@arm.com>

--- test_conformance/integer_ops/main.cpp
+++ test_conformance/integer_ops/main.cpp
@@ -26,14 +26,32 @@
 
 void fill_test_values( cl_long *outBufferA, cl_long *outBufferB, size_t numElements, MTdata d )
 {
-    static const cl_long sUniqueValues[] = { (cl_long)0x3333333333333333LL, (cl_long)0x5555555555555555LL, (cl_long)0x9999999999999999LL, (cl_long)0xaaaaaaaaaaaaaaaaLL, (cl_long)0xccccccccccccccccLL,
-        (cl_long)0x3030303030303030LL, (cl_long)0x5050505050505050LL, (cl_long)0x9090909090909090LL,  (cl_long)0xa0a0a0a0a0a0a0a0LL, (cl_long)0xc0c0c0c0c0c0c0c0LL, (cl_long)0xf0f0f0f0f0f0f0f0LL,
-        (cl_long)0x0303030303030303LL, (cl_long)0x0505050505050505LL, (cl_long)0x0909090909090909LL,  (cl_long)0x0a0a0a0a0a0a0a0aLL, (cl_long)0x0c0c0c0c0c0c0c0cLL, (cl_long)0x0f0f0f0f0f0f0f0fLL,
-        (cl_long)0x3300330033003300LL, (cl_long)0x5500550055005500LL, (cl_long)0x9900990099009900LL,  (cl_long)0xaa00aa00aa00aa00LL, (cl_long)0xcc00cc00cc00cc00LL, (cl_long)0xff00ff00ff00ff00LL,
-        (cl_long)0x0033003300330033LL, (cl_long)0x0055005500550055LL, (cl_long)0x0099009900990099LL,  (cl_long)0x00aa00aa00aa00aaLL, (cl_long)0x00cc00cc00cc00ccLL, (cl_long)0x00ff00ff00ff00ffLL,
-        (cl_long)0x3333333300000000LL, (cl_long)0x5555555500000000LL, (cl_long)0x9999999900000000LL,  (cl_long)0xaaaaaaaa00000000LL, (cl_long)0xcccccccc00000000LL, (cl_long)0xffffffff00000000LL,
-        (cl_long)0x0000000033333333LL, (cl_long)0x0000000055555555LL, (cl_long)0x0000000099999999LL,  (cl_long)0x00000000aaaaaaaaLL, (cl_long)0x00000000ccccccccLL, (cl_long)0x00000000ffffffffLL,
-        (cl_long)0x3333000000003333LL, (cl_long)0x5555000000005555LL, (cl_long)0x9999000000009999LL,  (cl_long)0xaaaa00000000aaaaLL, (cl_long)0xcccc00000000ccccLL, (cl_long)0xffff00000000ffffLL};
+    static const cl_long sUniqueValues[] = {
+        (cl_long)0x3333333333333333LL, (cl_long)0x5555555555555555LL,
+        (cl_long)0x9999999999999999LL, (cl_long)0xaaaaaaaaaaaaaaaaLL,
+        (cl_long)0xccccccccccccccccLL, (cl_long)0x3030303030303030LL,
+        (cl_long)0x5050505050505050LL, (cl_long)0x9090909090909090LL,
+        (cl_long)0xa0a0a0a0a0a0a0a0LL, (cl_long)0xc0c0c0c0c0c0c0c0LL,
+        (cl_long)0xf0f0f0f0f0f0f0f0LL, (cl_long)0x0303030303030303LL,
+        (cl_long)0x0505050505050505LL, (cl_long)0x0909090909090909LL,
+        (cl_long)0x0a0a0a0a0a0a0a0aLL, (cl_long)0x0c0c0c0c0c0c0c0cLL,
+        (cl_long)0x0f0f0f0f0f0f0f0fLL, (cl_long)0x3300330033003300LL,
+        (cl_long)0x5500550055005500LL, (cl_long)0x9900990099009900LL,
+        (cl_long)0xaa00aa00aa00aa00LL, (cl_long)0xcc00cc00cc00cc00LL,
+        (cl_long)0xff00ff00ff00ff00LL, (cl_long)0x0033003300330033LL,
+        (cl_long)0x0055005500550055LL, (cl_long)0x0099009900990099LL,
+        (cl_long)0x00aa00aa00aa00aaLL, (cl_long)0x00cc00cc00cc00ccLL,
+        (cl_long)0x00ff00ff00ff00ffLL, (cl_long)0x3333333300000000LL,
+        (cl_long)0x5555555500000000LL, (cl_long)0x9999999900000000LL,
+        (cl_long)0xaaaaaaaa00000000LL, (cl_long)0xcccccccc00000000LL,
+        (cl_long)0xffffffff00000000LL, (cl_long)0x0000000033333333LL,
+        (cl_long)0x0000000055555555LL, (cl_long)0x0000000099999999LL,
+        (cl_long)0x00000000aaaaaaaaLL, (cl_long)0x00000000ccccccccLL,
+        (cl_long)0x00000000ffffffffLL, (cl_long)0x3333000000003333LL,
+        (cl_long)0x5555000000005555LL, (cl_long)0x9999000000009999LL,
+        (cl_long)0xaaaa00000000aaaaLL, (cl_long)0xcccc00000000ccccLL,
+        (cl_long)0xffff00000000ffffLL
+    };
     static cl_long sSpecialValues[ 128 + 128 + 128 + ( sizeof( sUniqueValues ) / sizeof( sUniqueValues[ 0 ] ) ) ] = { 0 };
 
     if( sSpecialValues[ 0 ] == 0 )
