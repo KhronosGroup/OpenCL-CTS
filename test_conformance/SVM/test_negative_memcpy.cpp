78948db8f2e4a0def06b446f8b2c2b974d075049
Marcin Hajder <marcin.hajder@gmail.com>
2025-12-05 12:58:10 +0100

    Added tests validating that clEnqueueSVMMemcpy returns CL_MEM_COPY_OVERLAP when source and destination SVM ranges overlap

---
+++ test_conformance/SVM/test_negative_memcpy.cpp
@@ -0,0 +1,180 @@
+//
+// Copyright (c) 2025 The Khronos Group Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#include "common.h"
+
+REGISTER_TEST(svm_negative_dst_overlap_src)
+{
+    cl_int err = CL_SUCCESS;
+
+    cl_device_svm_capabilities caps = 0;
+    err = clGetDeviceInfo(device, CL_DEVICE_SVM_CAPABILITIES, sizeof(caps),
+                          &caps, NULL);
+
+    if (err != CL_SUCCESS || caps == 0)
+    {
+        log_error("svm_negative_dst_overlap_src: SVM is not supported on this "
+                  "device\n");
+        return TEST_SKIP;
+    }
+
+    clContextWrapper contextWrapper = NULL;
+    clCommandQueueWrapper queues[MAXQ];
+    cl_uint num_devices = 0;
+    err = create_cl_objects(device, NULL, &contextWrapper, NULL, &queues[0],
+                            &num_devices, 0);
+    test_error(err, "svm_negative_dst_overlap_src: create_cl_objects failed");
+
+    context = contextWrapper;
+    queue = queues[0];
+
+    const size_t dataSize = 32;
+
+    char *data_buf =
+        (char *)clSVMAlloc(contextWrapper, CL_MEM_READ_WRITE, dataSize * 2, 0);
+    if (!data_buf)
+    {
+        log_error(
+            "svm_negative_dst_overlap_src: invalid result for clSVMAlloc\n");
+        return TEST_FAIL;
+    }
+
+    char *src = data_buf;
+    char *dst = src + dataSize / 2;
+
+    err = clEnqueueSVMMemcpy(queue, CL_TRUE, dst, src, dataSize, 0, NULL, NULL);
+
+    clSVMFree(context, data_buf);
+
+    if (err != CL_MEM_COPY_OVERLAP)
+    {
+        log_error("svm_negative_dst_overlap_src: invalid result for "
+                  "clEnqueueSVMMemcpy, expected CL_MEM_COPY_OVERLAP, got %s\n",
+                  IGetErrorString(err));
+        return TEST_FAIL;
+    }
+
+    return TEST_PASS;
+}
+
+REGISTER_TEST(svm_negative_src_overlap_dst)
+{
+    cl_int err = CL_SUCCESS;
+
+    cl_device_svm_capabilities caps = 0;
+    err = clGetDeviceInfo(device, CL_DEVICE_SVM_CAPABILITIES, sizeof(caps),
+                          &caps, NULL);
+
+    if (err != CL_SUCCESS || caps == 0)
+    {
+        log_error("svm_negative_dst_overlap_src: SVM is not supported on this "
+                  "device\n");
+        return TEST_SKIP;
+    }
+
+    clContextWrapper contextWrapper = NULL;
+    clCommandQueueWrapper queues[MAXQ];
+    cl_uint num_devices = 0;
+    err = create_cl_objects(device, NULL, &contextWrapper, NULL, &queues[0],
+                            &num_devices, 0);
+    test_error(err, "svm_negative_src_overlap_dst: create_cl_objects failed");
+
+    context = contextWrapper;
+    queue = queues[0];
+
+    const size_t dataSize = 32;
+
+    char *data_buf =
+        (char *)clSVMAlloc(context, CL_MEM_READ_WRITE, dataSize * 2, 0);
+    if (!data_buf)
+    {
+        log_error(
+            "svm_negative_src_overlap_dst: invalid result for clSVMAlloc\n");
+        return TEST_FAIL;
+    }
+
+    char *dst = data_buf;
+    char *src = dst + dataSize / 2;
+
+    err = clEnqueueSVMMemcpy(queue, CL_TRUE, dst, src, dataSize, 0, NULL, NULL);
+
+    if (err != CL_MEM_COPY_OVERLAP)
+    {
+        clSVMFree(context, data_buf);
+        log_error("svm_negative_src_overlap_dst: invalid result for "
+                  "clEnqueueSVMMemcpy, expected CL_MEM_COPY_OVERLAP, got %s\n",
+                  IGetErrorString(err));
+        return TEST_FAIL;
+    }
+
+    clSVMFree(context, data_buf);
+
+    return TEST_PASS;
+}
+
+REGISTER_TEST(svm_negative_src_total_overlap)
+{
+    cl_int err = CL_SUCCESS;
+
+    cl_device_svm_capabilities caps = 0;
+    err = clGetDeviceInfo(device, CL_DEVICE_SVM_CAPABILITIES, sizeof(caps),
+                          &caps, NULL);
+
+    if (err != CL_SUCCESS || caps == 0)
+    {
+        log_error("svm_negative_dst_overlap_src: SVM is not supported on this "
+                  "device\n");
+        return TEST_SKIP;
+    }
+
+    clContextWrapper contextWrapper = NULL;
+    clCommandQueueWrapper queues[MAXQ];
+    cl_uint num_devices = 0;
+    err = create_cl_objects(device, NULL, &contextWrapper, NULL, &queues[0],
+                            &num_devices, 0);
+    test_error(err, "svm_negative_src_total_overlap: create_cl_objects failed");
+
+    context = contextWrapper;
+    queue = queues[0];
+
+    const size_t dataSize = 32;
+
+    char *data_buf =
+        (char *)clSVMAlloc(context, CL_MEM_READ_WRITE, dataSize, 0);
+    if (!data_buf)
+    {
+        log_error(
+            "svm_negative_src_total_overlap: invalid result for clSVMAlloc\n");
+        return TEST_FAIL;
+    }
+
+    char *src = data_buf;
+    char *dst = data_buf;
+
+    err = clEnqueueSVMMemcpy(queue, CL_TRUE, dst, src, dataSize, 0, NULL, NULL);
+
+    if (err != CL_MEM_COPY_OVERLAP)
+    {
+        clSVMFree(context, data_buf);
+        log_error("svm_negative_src_total_overlap: invalid result for "
+                  "clEnqueueSVMMemcpy, expected CL_MEM_COPY_OVERLAP, got %s\n",
+                  IGetErrorString(err));
+        return TEST_FAIL;
+    }
+
+    clSVMFree(context, data_buf);
+
+    return TEST_PASS;
+}
