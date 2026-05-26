KhronosGroup/OpenCL-CTS

commit 6258796eb6e08fd149f06dfbf6001d7a9bbc1338

Fix test_non_uniform_work_group on devices that do no support it (#2598)
On devices that do not support non-uniform workgroups, the test was
using incorrect round-up values for the global work group sizes. The
global work group sizes were round up such that they are multiples of
the local sizes, but those local sizes were later adjusted, in the call
to `prepareDevice`, causing the test to (correctly) fail with the error
code `CL_INVALID_WORK_GROUP_SIZE`, because the global work group sizes
are no longer multiples of the local ones.

Example log before the fix, running `non_uniform_2d_basic`:
```
Rounding globalSize[0] = 42967 -> 43243
Rounding globalSize[1] = 13 -> 14
T E S T  P A R A M E T E R S :
    Number of dimensions:    2
    Global work group size:    {43243, 14}
    Local work group size:    {256, 1}
```
Note how the adjusted global size (43243) is not a multiple of the
adjusted local size (256).

Example log after the fix, running the same test:
```
Rounding _globalSize[0] = 42967 -> 43008
Rounding _globalSize[1] = 13 -> 14
T E S T  P A R A M E T E R S :
    Number of dimensions:    2
    Global work group size:    {43008, 14}
    Local work group size:    {256, 1}
```
Adjusted global size is now a multiple of the local size.

Signed-off-by: Ahmed Hesham <ahmed.hesham@arm.com>

test_conformance/non_uniform_work_group/TestNonUniformWorkGroup.cpp
@@ -261,50 +261,6 @@ TestNonUniformWorkGroup::TestNonUniformWorkGroup(
         return;
     }
 
-    // For OpenCL-3.0 support for non-uniform workgroups is optional, it's still
-    // useful to run these tests since we can verify the behavior of the
-    // get_enqueued_local_size() builtin for uniform workgroups, so we round up
-    // the global size to insure uniform workgroups on those 3.0 devices.
-    // We only need to do this when localSize is non-null, otherwise the driver
-    // will select a value for localSize which will be uniform on devices that
-    // don't support non-uniform work-groups.
-    if (nullptr != localSize && get_device_cl_version(device) >= Version(3, 0))
-    {
-        // Query for the non-uniform work-group support.
-        cl_bool are_non_uniform_sub_groups_supported{ CL_FALSE };
-        auto error =
-            clGetDeviceInfo(device, CL_DEVICE_NON_UNIFORM_WORK_GROUP_SUPPORT,
-                            sizeof(are_non_uniform_sub_groups_supported),
-                            &are_non_uniform_sub_groups_supported, nullptr);
-        if (error)
-        {
-            print_error(error,
-                        "clGetDeviceInfo failed for "
-                        "CL_DEVICE_NON_UNIFORM_WORK_GROUP_SUPPORT");
-            // This signals an error to the caller (see above).
-            _globalSize[0] = 0;
-            return;
-        }
-
-        // If non-uniform work-groups are not supported round up the global
-        // sizes so workgroups are uniform and we have at least one.
-        if (CL_FALSE == are_non_uniform_sub_groups_supported)
-        {
-            log_info(
-                "WARNING: Non-uniform work-groups are not supported on this "
-                "device.\n Running test with uniform work-groups.\n");
-            for (unsigned dim = 0; dim < dims; ++dim)
-            {
-                auto global_size_before = globalSize[dim];
-                auto global_size_rounded = global_size_before
-                    + (localSize[dim] - global_size_before % localSize[dim]);
-                globalSize[dim] = global_size_rounded;
-                log_info("Rounding globalSize[%d] = %zu -> %zu\n", dim,
-                         global_size_before, global_size_rounded);
-            }
-        }
-    }
-
     cl_uint i;
     _globalWorkOffset_IsNull = true;
     _localSize_IsNull = true;
@@ -536,6 +492,48 @@ int TestNonUniformWorkGroup::prepareDevice () {
     if(_enqueuedLocalSize[i] > _maxWorkItemSizes[i]) {
       _enqueuedLocalSize[i] = _maxWorkItemSizes[i];
     }
+  }
+
+  // For OpenCL-3.0 support for non-uniform workgroups is optional, it's still
+  // useful to run these tests since we can verify the behavior of the
+  // get_enqueued_local_size() builtin for uniform workgroups, so we round up
+  // the global size to insure uniform workgroups on those 3.0 devices.
+  // We only need to do this when localSize is non-null, otherwise the driver
+  // will select a value for localSize which will be uniform on devices that
+  // don't support non-uniform work-groups.
+  if (get_device_cl_version(_device) >= Version(3, 0))
+  {
+      // Query for the non-uniform work-group support.
+      cl_bool are_non_uniform_sub_groups_supported{ CL_FALSE };
+      auto error =
+          clGetDeviceInfo(_device, CL_DEVICE_NON_UNIFORM_WORK_GROUP_SUPPORT,
+                          sizeof(are_non_uniform_sub_groups_supported),
+                          &are_non_uniform_sub_groups_supported, nullptr);
+      if (error)
+      {
+          print_error(error,
+                      "clGetDeviceInfo failed for "
+                      "CL_DEVICE_NON_UNIFORM_WORK_GROUP_SUPPORT");
+          return -1;
+      }
+
+      // If non-uniform work-groups are not supported round up the global
+      // sizes so workgroups are uniform and we have at least one.
+      if (CL_FALSE == are_non_uniform_sub_groups_supported)
+      {
+          log_info("WARNING: Non-uniform work-groups are not supported on this "
+                   "device.\n Running test with uniform work-groups.\n");
+          for (unsigned dim = 0; dim < _dims; ++dim)
+          {
+              auto global_size_before = _globalSize[dim];
+              auto global_size_rounded = global_size_before
+                  + (_enqueuedLocalSize[dim]
+                     - global_size_before % _enqueuedLocalSize[dim]);
+              _globalSize[dim] = global_size_rounded;
+              log_info("Rounding _globalSize[%d] = %zu -> %zu\n", dim,
+                       global_size_before, global_size_rounded);
+          }
+      }
   }
 
   if(_localSize_IsNull == false)

commit ce2eb90dfd46bd7e5331b6dfae184a7374a469e2

Fix non-uniform workgroups atomics tests (#2702)
On devices that don't support non-uniform workgroups, recompute
`_numOfGlobalWorkItems` after any global-size adjustments.

This fixes the issue introduced by #2598.

Signed-off-by: Ahmed Hesham <ahmed.hesham@arm.com>

test_conformance/non_uniform_work_group/TestNonUniformWorkGroup.cpp
@@ -536,6 +536,8 @@ int TestNonUniformWorkGroup::prepareDevice () {
       }
   }
 
+  _numOfGlobalWorkItems = _globalSize[0] * _globalSize[1] * _globalSize[2];
+
   if(_localSize_IsNull == false)
     calculateExpectedValues();
 