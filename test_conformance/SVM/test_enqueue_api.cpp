31315f2c8b7c003deebea7f5cd702caaf7e033f9
Julia Jiang <56359287+jujiang-del@users.noreply.github.com>
2026-09-23 15:36:35 -0400

    Update test_enqueue_api.cpp

--- test_conformance/SVM/test_enqueue_api.cpp
+++ test_conformance/SVM/test_enqueue_api.cpp
@@ -18,6 +18,8 @@
 
 #include <vector>
 #include <atomic>
+#include <chrono>
+#include <thread>
 
 #if !defined(_WIN32)
 #include <unistd.h>
@@ -323,11 +325,7 @@ REGISTER_TEST(svm_enqueue_api)
     // wait for the callback
     while (data.status.load(std::memory_order_acquire) == 0)
     {
-#ifdef _WIN32
-        Sleep(1);
-#else
-        usleep(1);
-#endif
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 
     // check if number of SVM pointers returned in the callback matches with
